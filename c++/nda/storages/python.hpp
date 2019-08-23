933e1ea07fae3712311f15ac9fa935de91939160
Olivier Parcollet <olivier.parcollet@cea.fr>
2019-08-23 09:44:25 -0400

    compile idxmap test. storage test to be cleaned

--- c++/nda/storages/python.hpp
+++ c++/nda/storages/python.hpp
@@ -21,13 +21,10 @@
  *
  ******************************************************************************/
 #pragma once
-#include <cpp2py/pyref.hpp>
 
 // SHOULD ONLY BE INCLUDED in a python module.
-#ifndef PYTHON_NUMPY_VERSION_LT_17
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#endif
-#include "Python.h"
+
+#include <Python.h>
 #include <numpy/arrayobject.h>
 #include <cpp2py/pyref.hpp>
 
@@ -45,38 +42,42 @@ namespace nda::mem {
 
     if (obj == NULL) throw std::runtime_error(" Can not build an mem_blk_handle from a NULL PyObject *");
     if (!PyArray_Check(obj)) throw std::runtime_error("Internal error : ref_counter construct from pyo : obj is not an array");
-    Py_INCREF(obj); // assume borrowed
+
+    // We create a new shared handle -> increase refcount
+    Py_INCREF(obj);
+
     PyArrayObject *arr = (PyArrayObject *)(obj);
 
-    handle<T, 'S'> r; // empty
-    r.data        = (T *)PyArray_DATA(arr);
-    r.size        = size_t(PyArray_SIZE(arr));
-    r.id          = globals::rtable.get();
-    r.sptr        = obj;
-    r.release_fnt = (void*)&py_decref;
+    handle<T, 'S'> r{
+       (T *)PyArray_DATA(arr),    // r._data
+       size_t(PyArray_SIZE(arr)), // r._size
+       obj,                       // r._foreign_handle
+       (void *)&py_decref         // r._foreign_decref
+    };
     return r;
   }
 
-  // ------------------  make_pycapsule  ----------------------------------------------------
-  // make a pycapsule out of the shared handle to return to Python
+  // ------------------  delete_pycapsule  ----------------------------------------------------
 
+  // Properly delete the handle<T, 'S'> in a PyCapsule
   template <typename T> static void delete_pycapsule(PyObject *capsule) {
     handle<T, 'S'> *handle = static_cast<nda::mem::handle<T, 'S'> *>(PyCapsule_GetPointer(capsule, "guard"));
-    std::cerr << "decapsulate : nrefs" << handle->nref() << "\n";
-    //handle->decref();
+    //std::cerr << "decapsulate : "<< handle->id << "  "<< handle->data << "  nrefs" << handle->nref() << "\n";
     delete handle;
   }
 
-  template <typename T> PyObject *make_pycapsule(handle<T, 'S'> h) {
-    //h.incref();
-    std::cerr << "capsulate : nrefs" << h.nref() << "\n";
-    void *keep = new handle<T, 'S'>{std::move(h)}; // a new reference
+  // ------------------  make_pycapsule,   ----------------------------------------------------
 
+  // Make a pycapsule out of the shared handle to return to Python
+  template <typename T> PyObject *make_pycapsule(handle<T, 'R'>  const &h) {
+    void *keep = new handle<T, 'S'>{h}; // a new reference
     return PyCapsule_New(keep, "guard", &delete_pycapsule<T>);
   }
 
-  template <typename T> PyObject *make_pycapsule(handle<T, 'R'> const &h) { return make_pycapsule(handle<T, 'S'>{h}); }
-
-  template <typename T> PyObject *make_pycapsule(handle<T, 'B'> const &h) = delete; // Can not return a borrowed view to Python
+  template <typename T> PyObject *make_pycapsule(handle<T, 'B'> const &h) {
+    if (h.parent() == nullptr) throw std::runtime_error("Can not return to python a view on something else than an nda::array");
+    void *keep = new handle<T, 'S'>{*h.parent()}; // a new reference
+    return PyCapsule_New(keep, "guard", &delete_pycapsule<T>);
+  }
 
 } // namespace nda::mem
