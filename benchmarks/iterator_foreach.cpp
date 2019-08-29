7c79939a592436b030ea6f15a21e0031d42701d3
Olivier Parcollet <olivier.parcollet@cea.fr>
2019-08-29 12:27:45 -0400

    bench iterator

--- benchmarks/iterator_foreach.cpp
+++ benchmarks/iterator_foreach.cpp
@@ -13,10 +13,10 @@ static void for1(benchmark::State &state) {
   nda::array<double, 1> a(N1);
 
   const long l0 = a.indexmap().lengths()[0];
-  
+
   while (state.KeepRunning()) {
- //   for (long i=0; i <a.indexmap().lengths()[0]; ++i) { benchmark::DoNotOptimize(a(i) = 10 * i); }
-    for (long i=0; i <l0; ++i) { benchmark::DoNotOptimize(a(i) = 10 * i); }
+    //   for (long i=0; i <a.indexmap().lengths()[0]; ++i) { benchmark::DoNotOptimize(a(i) = 10 * i); }
+    for (long i = 0; i < l0; ++i) { benchmark::DoNotOptimize(a(i) = 10 * i); }
   }
 }
 BENCHMARK(for1);
@@ -46,7 +46,7 @@ static void pointer_1A(benchmark::State &state) {
 
   while (state.KeepRunning()) {
     double *__restrict__ p = &(a(0));
-    for (long i0 = 0; i0 < l0; ++i0) benchmark::DoNotOptimize(p[i0 * a.indexmap().strides()[0]] = 10 * i0);
+    for (long i0 = 0; i0 < l0; ++i0) benchmark::DoNotOptimize(p[a.indexmap().offset() + i0 * a.indexmap().strides()[0]] = 10 * i0);
   }
 }
 BENCHMARK(pointer_1A);
@@ -59,12 +59,24 @@ static void pointer_1B(benchmark::State &state) {
 
   while (state.KeepRunning()) {
     double *__restrict__ p = &(a(0));
-    for (long i0 = 0; i0 < l0; ++i0) benchmark::DoNotOptimize(p[i0 ] = 10 * i0);
+    for (long i0 = 0; i0 < l0; ++i0) benchmark::DoNotOptimize(p[i0 * s0] = 10 * i0);
   }
 }
 BENCHMARK(pointer_1B);
 
 // ------------------------------- 2d ----------------------------------------
+
+static void for2(benchmark::State &state) {
+  nda::array<double, 2> a(N1, N2);
+  const long l0 = a.indexmap().lengths()[0];
+  const long l1 = a.indexmap().lengths()[1];
+
+  while (state.KeepRunning()) {
+    for (long i = 0; i < l0; ++i)
+      for (long j = 0; j < l1; ++j) { benchmark::DoNotOptimize(a(i, j) = 10 * i + j); }
+  }
+}
+BENCHMARK(for2);
 
 static void foreach2(benchmark::State &state) {
   nda::array<double, 2> a(N1, N2);
@@ -90,9 +102,10 @@ static void pointer_2A(benchmark::State &state) {
   const long l1 = a.indexmap().lengths()[1];
 
   while (state.KeepRunning()) {
-    double *__restrict__ p = &(a(0,0));
+    double *__restrict__ p = &(a(0, 0));
     for (long i0 = 0; i0 < l0; ++i0)
-      for (long i1 = 0; i1 < l1; ++i1) benchmark::DoNotOptimize(p[i0 * a.indexmap().strides()[0] + i1 * a.indexmap().strides()[1]] = 10 * i0 + i1);
+      for (long i1 = 0; i1 < l1; ++i1)
+        benchmark::DoNotOptimize(p[a.indexmap().offset() + i0 * a.indexmap().strides()[0] + i1 * a.indexmap().strides()[1]] = 10 * i0 + i1);
   }
 }
 BENCHMARK(pointer_2A);
@@ -100,18 +113,32 @@ BENCHMARK(pointer_2A);
 static void pointer_2B(benchmark::State &state) {
   nda::array<double, 2> a(N1, N2);
 
-  const long s0 = a.indexmap().strides()[0];
-  const long s1 = a.indexmap().strides()[1];
+  const long s0     = a.indexmap().strides()[0];
+  const long s1     = a.indexmap().strides()[1];
+  const long l0     = a.indexmap().lengths()[0];
+  const long l1     = a.indexmap().lengths()[1];
+  const long offset = a.indexmap().offset();
+
+  while (state.KeepRunning()) {
+    double *__restrict__ p = &(a(0, 0));
+    for (long i0 = 0; i0 < l0; ++i0)
+      for (long i1 = 0; i1 < l1; ++i1) benchmark::DoNotOptimize(p[offset + i0 * s0 + i1 * s1] = 10 * i0 + i1);
+  }
+}
+BENCHMARK(pointer_2B);
+
+static void pointer_2C(benchmark::State &state) {
+  nda::array<double, 2> a(N1, N2);
+
   const long l0 = a.indexmap().lengths()[0];
   const long l1 = a.indexmap().lengths()[1];
 
   while (state.KeepRunning()) {
-    double *__restrict__ p = &(a(0,0));
-    for (long i0 = 0; i0 < l0; ++i0)
-      for (long i1 = 0; i1 < l1; ++i1) benchmark::DoNotOptimize(p[i0 * s0 + i1 * s1] = 10 * i0 + i1);
+    double *__restrict__ p = &(a(0, 0));
+    for (long i = 0; i < l0 * l1; ++i) benchmark::DoNotOptimize(p[i] = 10 * i);
   }
 }
-BENCHMARK(pointer_2B);
+BENCHMARK(pointer_2C);
 
 /*
 static void foreach_manual_simple1(benchmark::State &state) {
