7cd4f40d7bc51bfa38035cc873c99c9e9d307517
Olivier Parcollet <olivier.parcollet@cea.fr>
2019-09-01 09:21:07 -0400

    WIP

---
+++ c++/nda/algorithms.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+namespace nda {
+
+  // FIXME ? For some generic code ? needed ? Cf TRIQS and comment
+  //inline double max_element(double x) { return x; }
+  //inline std::complex<double> max_element(std::complex<double> x) { return x; }
+
+  /**
+   * @tparam A Anything modeling NdArray
+   * @param a The object of type A
+   * @return The maximum element of A
+   */
+  template <typename A>
+  auto max_element(A const &a) REQUIRES(is_ndarray_v<A>) {
+    return fold([](auto const &a, auto const &b) {
+      using std::max;
+      return max(a, b);
+    })(a, get_first_element(a));
+  }
+
+  /**
+   * @tparam A Anything modeling NdArray
+   * @param a The object of type A
+   * @return The minimum element of A
+   */
+  template <typename A>
+  auto min_element(A const &a) REQUIRES(is_ndarray_v<A>) {
+    return fold([](auto const &a, auto const &b) {
+      using std::min;
+      return min(a, b);
+    })(a, get_first_element(a));
+  }
+
+  /**
+   * @tparam A Anything modeling NdArray
+   * @param a The object of type A
+   * @return The sum of all elements of a 
+   */
+
+  template <typename A>
+  auto sum(A const &a) REQUIRES(is_ndarray_v<A>) {
+    return fold(std::plus<>{}, a);
+  }
+
+} // namespace nda
+
---
+++ c++/nda/fold.hpp
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ *
+ * TRIQS: a Toolbox for Research in Interacting Quantum Systems
+ *
+ * Copyright (C) 2012 by O. Parcollet
+ *
+ * TRIQS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * TRIQS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * TRIQS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#pragma once
+
+namespace nda {
+
+  /**
+   * @tparam A
+   * @tparam F is a function f(x, r)
+   * @tparam R
+   * @param f
+   * @param a
+   * @param r
+   *
+   * fold computes f(f(r, a(0,0)), a(0,1), ...)  etc
+   */ 
+  template <typename A, typename F, typename R = get_value_t<A>>
+  auto fold(F f, A const&a, R r= R{}) {
+    decltype(f(r, get_value_t<A>{})) r2 = r;
+    // to take into account that f may be double,double -> double, while one passes 0 (an int...)
+    // R = int, R2= double in such case, and the result will be a double, or narrowing will occur
+    nda::foreach (a.indexmap().lengths(), [&a,&r2](auto &&... args) { r2 = f(r2, a(args...)); });
+    return r2;
+  }
+
+} // namespace nda
