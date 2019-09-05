d5948ac191469d416a337a84d98a447278d30b4b
Olivier Parcollet <olivier.parcollet@cea.fr>
2019-09-05 19:00:22 -0400

    except ok

--- test/c++/nda_bound_check.cpp
+++ test/c++/nda_bound_check.cpp
@@ -5,20 +5,11 @@
 TEST(Array, BoundCheck) {
 
   nda::array<long, 2> A(2, 3);
-//EXPECT_THROW(throw std::runtime_error("LL"), std::runtime_error);
-
-try { 
-// throw std::runtime_error("LL");
- A(0,3);
-} 
-catch (...) { std::cout  << " GOT IT "<< std::endl;}
-
-
-// EXPECT_THROW(A(0, 3), nda::details::my_runtime_error); //, std::runtime_error);
-  //EXPECT_THROW(A(nda::range(0, 4), 2), std::runtime_error);
-  //EXPECT_THROW(A(nda::range(10, 14), 2), std::runtime_error);
-  //EXPECT_THROW(A(nda::range(), 5), std::runtime_error);
-  //EXPECT_THROW(A(0, 3), std::runtime_error);
-//
+ 
+  EXPECT_THROW(A(0, 3), std::runtime_error); //, std::runtime_error);
+  EXPECT_THROW(A(nda::range(0, 4), 2), std::runtime_error);
+  EXPECT_THROW(A(nda::range(10, 14), 2), std::runtime_error);
+  EXPECT_THROW(A(nda::range(), 5), std::runtime_error);
+  EXPECT_THROW(A(0, 3), std::runtime_error);
 }
 MAKE_MAIN;
