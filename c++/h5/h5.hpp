2ba4adeaf075e65b010ee51730767c5de30f435b
Olivier Parcollet <olivier.parcollet@cea.fr>
2019-09-07 07:57:34 -0400

    new test (2)

--- c++/h5/h5.hpp
+++ c++/h5/h5.hpp
@@ -34,6 +34,19 @@
 
 //#include "./h5/stl/variant.hpp"
 
+// Correction of a bug in STL : 
+namespace std { // has to be in the right namespace for ADL !
+
+  template <typename T> std::complex<T> operator+(std::complex<T> const &a, long b) { return a + T(b); }
+  template <typename T> std::complex<T> operator+(long a, std::complex<T> const &b) { return T(a) + b; }
+  template <typename T> std::complex<T> operator-(std::complex<T> const &a, long b) { return a - T(b); }
+  template <typename T> std::complex<T> operator-(long a, std::complex<T> const &b) { return T(a) - b; }
+  template <typename T> std::complex<T> operator*(std::complex<T> const &a, long b) { return a * T(b); }
+  template <typename T> std::complex<T> operator*(long a, std::complex<T> const &b) { return T(a) * b; }
+  template <typename T> std::complex<T> operator/(std::complex<T> const &a, long b) { return a / T(b); }
+  template <typename T> std::complex<T> operator/(long a, std::complex<T> const &b) { return T(a) / b; }
+} // namespace std
+
 // FIXME : Why all these include by default ?
 
 // FIXME : Still needed ?
