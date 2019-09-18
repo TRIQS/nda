781ac501f4891b57ab69360b877b13e832682c34
Olivier Parcollet <olivier.parcollet@cea.fr>
2019-09-18 10:12:16 -0400

    lapack1

--- c++/nda/lapack/getri.hpp
+++ c++/nda/lapack/getri.hpp
@@ -27,30 +27,35 @@
 #include "tools.hpp"
 #include "qcache.hpp"
 
-namespace triqs::arrays::lapack {
+namespace nda::lapack {
 
-  using namespace blas_lapack_tools;
-
-  inline size_t r_round(double x) { return std::round(x) + 1; }
-  inline size_t r_round(std::complex<double> x) { return std::round(std::real(x)) + 1; }
   /**
-  * Calls getri on a matrix or view
-  * Takes care of making temporary copies if necessary
-  */
-  template <typename MT>
-  typename std::enable_if<is_blas_lapack_type<typename MT::value_type>::value, int>::type getri(MT &A, arrays::vector<int> &ipiv) {
-    //getri (MT & A, arrays::vector<int> & ipiv, arrays::vector<typename MT::value_type> & work ) {
-    reflexive_qcache<MT> Ca(A);
-    auto dm = std::min(first_dim(Ca()), second_dim(Ca()));
-    if (ipiv.size() < dm) TRIQS_RUNTIME_ERROR << "getri : error in ipiv size : found " << ipiv.size() << " while it should be at least" << dm;
+   * Calls getri on a matrix or view
+   * @tparam M
+   * @param m
+   * @param ipiv
+   */
+  template <typename M>
+  int getri(M &m, arrays::vector<int> &ipiv) {
+    static_assert(is_blas_lapack_v<M>, "Matrices must have the same element type and it must be double, complex ...");
+
+    auto Ca = reflexive_qcache(m);
+    auto dm = std::min(Ca().extent(0), Ca().extent(1));
+    if (ipiv.size() < dm) NDA_RUNTIME_ERROR << "getri : error in ipiv size : found " << ipiv.size() << " while it should be at least" << dm;
+
     int info;
-    typename MT::value_type work1[2];
+    typename M::value_type work1[2];
     // first call to get the optimal lwork
     f77::getri(get_n_rows(Ca()), Ca().data_start(), get_ld(Ca()), ipiv.data_start(), work1, -1, info);
-    int lwork = r_round(work1[0]);
-    //std::cerr << " Optimisation lwork "<< lwork<< std::endl;
-    arrays::vector<typename MT::value_type> work(lwork);
+    int lwork;
+    if constexpr (is_complex_v<typename M::value_type>)
+      lwork = std::round(std::real(x)) + 1;
+    else
+      lwork = std::round(x) + 1;
+
+    array<typename MT::value_type, 1> work(lwork);
+
     f77::getri(get_n_rows(Ca()), Ca().data_start(), get_ld(Ca()), ipiv.data_start(), work.data_start(), lwork, info);
     return info;
   }
-} // namespace triqs::arrays::lapack
+} // namespace nda::lapack
