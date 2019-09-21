de2b786416aedac567d5da42279ec9daf1512853
Olivier Parcollet <olivier.parcollet@cea.fr>
2019-09-21 18:06:34 -0400

    Forgot file

---
+++ c++/h5/python.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "./array_interface.hpp"
+
+namespace h5 {
+
+  // mae a module in pytriqs/archive, using the wrapping
+
+  // Missing AATRIBUTE
+
+  void h5_write_bare(group g, std::string const &name, py);
+//  void h5_write_bare(group g, std::string const &name, PyObject *ob);
+
+  PyObject * h5_read_bare(group g, std::string const &name);
+
+} // namespace h5
