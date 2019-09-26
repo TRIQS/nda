f0077f3fa41313a5307c6cfd297ad3a87120c975
Olivier Parcollet <olivier.parcollet@cea.fr>
2019-09-26 13:12:19 -0400

    Static array continued

--- test/c++/nda/nda_static.cpp
+++ test/c++/nda/nda_static.cpp
@@ -4,11 +4,13 @@
 
 TEST(StackArray, create) { //NOLINT
 
-  //  nda::basic_array<long, 2, nda::basic_layout<nda::encode(std::array{3, 3}), nda::C_stride_order<3>, nda::layout_prop_e::contiguous>, 'A', nda::stack>
   nda::stack_array<long, 2, nda::static_extents(3, 3)> a;
   nda::array<long, 2> d(3, 3);
 
   a = 3;
+  d = 3;
+  EXPECT_ARRAY_NEAR(a, d);
+
   for (int i = 0; i < 3; ++i)
     for (int j = 0; j < 3; ++j) {
       a(i, j) = i + 10 * j;
@@ -20,11 +22,49 @@ TEST(StackArray, create) { //NOLINT
   ac = a + d;
 
   NDA_PRINT(a.indexmap());
-  NDA_PRINT(ac);
-  NDA_PRINT(ac);
+  //NDA_PRINT(ac);
   NDA_PRINT(ac.indexmap());
 
   EXPECT_ARRAY_NEAR(a, d);
+}
+
+// ==============================================================
+
+TEST(StackArray, slice) { //NOLINT
+
+  nda::stack_array<long, 2, nda::static_extents(3, 3)> a;
+
+  a = 3;
+  for (int i = 0; i < 3; ++i)
+    for (int j = 0; j < 3; ++j) { a(i, j) = i + 10 * j; }
+
+  {
+    auto v = a(_, 1);
+
+    nda::array<long, 2> ad{a};
+    nda::array<long, 1> vd{v};
+
+    NDA_PRINT(v.indexmap());
+    NDA_PRINT(a);
+    NDA_PRINT(v);
+
+    EXPECT_ARRAY_NEAR(a, ad);
+    EXPECT_ARRAY_NEAR(v, vd);
+  }
+
+  {
+    auto v = a(1, _);
+
+    nda::array<long, 2> ad{a};
+    nda::array<long, 1> vd{v};
+
+    NDA_PRINT(v.indexmap());
+    NDA_PRINT(a);
+    NDA_PRINT(v);
+
+    EXPECT_ARRAY_NEAR(a, ad);
+    EXPECT_ARRAY_NEAR(v, vd);
+  }
 }
 
 // ==============================================================
