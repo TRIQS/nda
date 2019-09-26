394ca5dd032933505e34bed5ab1078b0582d9245
Olivier Parcollet <olivier.parcollet@cea.fr>
2019-09-25 22:25:48 -0400

    Fix bug in generic gemv

--- c++/clef/macros.hpp
+++ c++/clef/macros.hpp
@@ -15,6 +15,7 @@
 #define REQUIRES(...) requires(__VA_ARGS__)
 #endif
 
-#define FORCEINLINE __inline__ __attribute__((always_inline))
+#define DECL_AND_RETURN(...)                                                                                                                         \
+  ->decltype(__VA_ARGS__) { return __VA_ARGS__; }
 
 #endif
