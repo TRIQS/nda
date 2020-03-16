cdba470afaa9c752cf6aa7cefceee7d70257fc59
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-03-16 10:37:21 -0400

    Make nda compatible against Python 3

--- test/python/nda/nda_test_converters.py
+++ test/python/nda/nda_test_converters.py
@@ -2,34 +2,34 @@ import nda_py_converter_test as C
 #import nda_py_a as A
 import numpy as np
 a = np.array(((1.0,2), (3,4)))
-print a
+print(a)
 
 
 ### FIXME : make inittest
 
-print C.f(a,1,1)
+print(C.f(a,1,1))
 
 # self.assertRaises
 
 try:
   C.f(a, 1, 2)
 except:
-  print "error caught"
+  print("error caught")
 
-print C.ma(5)
+print(C.ma(5))
 
     
 a = C.make_A()  #C.A(7)
 v = a.get()
-print v
+print(v)
 v[0] *=10
-print v
+print(v)
 
 vc = a.get_c()
-print "vc = ", vc
+print("vc = ", vc)
 
 try:
     vc[0] *=10
 except:
-  print "error caught"
+  print("error caught")
 
