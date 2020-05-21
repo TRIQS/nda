9bea6384bc828afecc4ee32920dc47e7ce55b219
Olivier Parcollet <olivier.parcollet@cea.fr>
2020-05-20 23:10:51 -0400

    Finish assign change. Add test for concepts

---
+++ test/c++/nda/nda_concepts.cpp
@@ -0,0 +1,32 @@
+#include "./test_common.hpp"
+
+// ==============================================================
+
+// just compile time 
+
+TEST(NDA, Concept) { // NOLINT
+
+static_assert(!std::is_pod<nda::array<long, 2>>::value, "POD pb");
+static_assert(nda::is_scalar_for_v<int, matrix<std::complex<double>>> == 1, "oops");
+
+#if __cplusplus > 201703L
+
+using nda::ArrayOfRank;
+using nda::Array;
+
+static_assert(Array< nda::array<int,2>>, "INTERNAL");
+static_assert(ArrayOfRank< nda::array<int,2>, 2>, "INTERNAL");
+static_assert(not ArrayOfRank< nda::array<int,2>, 1>, "INTERNAL");
+static_assert(not ArrayOfRank< nda::array<int,2>, 3>, "INTERNAL");
+
+static_assert(Array< nda::array_view<int,2>>, "INTERNAL");
+static_assert(ArrayOfRank< nda::array_view<int,2>, 2>, "INTERNAL");
+static_assert(not ArrayOfRank< nda::array_view<int,2>, 1>, "INTERNAL");
+static_assert(not ArrayOfRank< nda::array_view<int,2>, 3>, "INTERNAL");
+
+
+#endif
+
+
+}
+
