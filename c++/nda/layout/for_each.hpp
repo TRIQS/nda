2f18ca6c73ddf34591795d8cb8ebaaef54f8f539
Olivier Parcollet <olivier.parcollet@cea.fr>
2020-06-15 19:18:57 -0400

    Correction for gcc
    - mutable and attribute are expected in a diff order in clang and gcc !?

--- c++/nda/layout/for_each.hpp
+++ c++/nda/layout/for_each.hpp
@@ -83,7 +83,14 @@ namespace nda {
         for (long i = 0; i < imax; ++i) {
           for_each_static_impl<I + 1, StaticExtents, StrideOrder>(
              idx_lengths,
-             [ i, f ](auto &&... x) __attribute__((always_inline)) mutable { return f(i, x...); }); // mutable since f itself can be mutable !
+             [ i, f ](auto &&... x)
+// Great: clang and gcc want the lambda mutable and attribute in a different order !:
+#ifdef __clang__
+                __attribute__((always_inline)) mutable
+#else
+                mutable __attribute__((always_inline))
+#endif
+             { return f(i, x...); }); // mutable since f itself can be mutable !
         }
       }
     }
