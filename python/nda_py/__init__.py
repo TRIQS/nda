b11304749daed4678bc3833dea0398cf3af4eae1
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-06-20 16:29:53 -0400

    Adjust nda_py/__init__.py for latest cpp2py version

--- python/nda_py/__init__.py
+++ python/nda_py/__init__.py
@@ -24,9 +24,8 @@ r"""
 DOC
 
 """
-from cpp2py import Cpp2pyInfoBase
 
-class Cpp2pyInfo(Cpp2pyInfoBase):
+class Cpp2pyInfo:
 
     table_imports = {
     }
@@ -35,8 +34,5 @@ class Cpp2pyInfo(Cpp2pyInfoBase):
       'nda::basic_array' : 'nda_py/cpp2py_converters.hpp',
       'nda::basic_array_view' : 'nda_py/cpp2py_converters.hpp',
     }
-
-def _get_cpp2py_wrapped_class_enums():
-    return {'module_name' : 'UNUSED', 'includes' : "['<triqs/cpp2py_converters.hpp>']"}
 
 __all__ = ['Cpp2pyInfo']
