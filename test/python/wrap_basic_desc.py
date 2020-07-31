399b84078b4f9cfcbe463153f3d16429b19749f1
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-07-31 14:28:52 -0400

    Generate Apache 2 Copyright headers and adjust LICENSE.txt

--- test/python/wrap_basic_desc.py
+++ test/python/wrap_basic_desc.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2020 Simons Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Generated automatically using the command :
 # c++2py wrap_basic.hpp -C nda_py -m wrap_basic -o wrap_basic --cxxflags="-std=c++17" --includes=./../../c++ --includes=./../../python --target_file_only
 from cpp2py.wrap_generator import *
