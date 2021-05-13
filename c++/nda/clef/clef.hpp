ed2dc18a1eabe265f36e4288c1d0e884192f9ce5
Nils Wentzell <nwentzell@flatironinstitute.org>
2021-05-13 16:51:05 -0400

    FIX Error in clef lazy function macros, properly forward args in function wrapper

--- c++/nda/clef/clef.hpp
+++ c++/nda/clef/clef.hpp
@@ -707,14 +707,16 @@ namespace nda::clef {
   * --------------------------------------------------------------------------------------------------- */
 #define CLEF_MAKE_FNT_LAZY(name)                                                                                                                     \
   template <typename... A>                                                                                                                           \
-  auto name(A &&... __a) CLEF_REQUIRES(nda::clef::is_any_lazy<A...>) {                                                                               \
-    return make_expr_call([](auto const &... __b) -> decltype(auto) { return name(__b...); }, std::forward<A>(__a)...);                              \
+  auto name(A &&...__a) CLEF_REQUIRES(nda::clef::is_any_lazy<A...>) {                                                                                \
+    return make_expr_call([](auto &&...__b) -> decltype(auto) { return name(std::forward<decltype(__b)>(__b)...); }, std::forward<A>(__a)...);       \
   }
 
 #define CLEF_IMPLEMENT_LAZY_METHOD(TY, name)                                                                                                         \
   template <typename... A>                                                                                                                           \
-  auto name(A &&... __a) CLEF_REQUIRES(is_any_lazy<A...>) {                                                                                          \
-    return make_expr_call([](auto &&__obj, auto &&... __b) -> decltype(auto) { return __obj.name(__b...); }, *this, std::forward<A>(__a)...);        \
+  auto name(A &&...__a) CLEF_REQUIRES(is_any_lazy<A...>) {                                                                                           \
+    return make_expr_call(                                                                                                                           \
+       [](auto &&__obj, auto &&...__b) -> decltype(auto) { return std::forward<decltype(__obj)>(__obj).name(std::forward<decltype(__b)>(__b)...); }, \
+       *this, std::forward<A>(__a)...);                                                                                                              \
   }
 
 #define CLEF_IMPLEMENT_LAZY_CALL(...)                                                                                                                \
