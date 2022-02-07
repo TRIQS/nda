7cec4d9b9a407ec8cab9a14b1e8aa6a8abb876db
gen_copyright <mail@server.com>
2022-02-07 17:09:47 -0500

    Regenerate Apache copyright headers

--- c++/nda/stdutil/complex.hpp
+++ c++/nda/stdutil/complex.hpp
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Authors: Olivier Parcollet, Nils Wentzell
 
 #ifndef STD_ADDONS_COMPLEX_H
 #define STD_ADDONS_COMPLEX_H
