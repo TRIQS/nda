3c9115dedc206b098ca61aa9d2b25031ed0fa945
Nils Wentzell <nwentzell@flatironinstitute.org>
2022-04-14 16:16:24 -0400

    Enable bound-checking for range-based array slicing FIX #22

--- test/c++/nda_bound_check.cpp
+++ test/c++/nda_bound_check.cpp
@@ -24,9 +24,8 @@ TEST(Array, BoundCheck) { //NOLINT
 
   EXPECT_THROW(A(0, 3), std::runtime_error); //NOLINT
 
-  // FIXME : SHOULD WE REALLY THROW ?
-  //EXPECT_THROW(A(nda::range(0, 4), 2), std::runtime_error);
-  //EXPECT_THROW(A(nda::range(10, 14), 2), std::runtime_error);
+  EXPECT_THROW(A(nda::range(0, 4), 2), std::runtime_error);   //NOLINT
+  EXPECT_THROW(A(nda::range(10, 14), 2), std::runtime_error); //NOLINT
 
   EXPECT_THROW(A(nda::range(), 5), std::runtime_error); //NOLINT
   EXPECT_THROW(A(0, 3), std::runtime_error);            //NOLINT
