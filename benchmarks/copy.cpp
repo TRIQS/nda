3333621239f1e04f37a2da6ecb64394e07af2bce
Nils Wentzell <nwentzell@flatironinstitute.org>
2022-04-20 14:16:37 -0400

    Add benchmark for array copy operations

---
+++ benchmarks/copy.cpp
@@ -0,0 +1,77 @@
+// Copyright (c) 2019-2020 Simons Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "./bench_common.hpp"
+
+using value_t = double;
+
+template <size_t Rank>
+using array_t = nda::array<value_t, Rank>;
+
+template <size_t Rank>
+using device_array_t = nda::basic_array<value_t, Rank, C_layout, 'A', nda::heap<mem::Device>>;
+
+long KBmin = 8;
+long KBmax = 1 << 15;
+
+template <typename Array>
+static void Copy(benchmark::State &state) {
+  long NBytes = state.range(0) * 1024;
+  long N      = NBytes / sizeof(value_t);
+  auto src    = Array{nda::rand<value_t>(N)};
+  auto dst    = Array{nda::zeros<value_t>(N)};
+  for (auto _ : state) { dst = src; }
+  state.SetBytesProcessed(state.iterations() * NBytes);
+  state.counters["processed"] = NBytes;
+}
+BENCHMARK_TEMPLATE(Copy, array_t<1>)->RangeMultiplier(8)->Range(KBmin, KBmax);
+BENCHMARK_TEMPLATE(Copy, device_array_t<1>)->RangeMultiplier(8)->Range(KBmin, KBmax);
+
+template <typename Array>
+static void Copy1DStrided(benchmark::State &state) {
+  long NBytes = state.range(0) * 1024;
+  long step   = 10;
+  long N      = step * NBytes / sizeof(value_t);
+  auto src    = Array{nda::rand<value_t>(N)};
+  auto dst    = Array{nda::zeros<value_t>(N)};
+  auto src_v  = src(range(0, N, step));
+  auto dst_v  = dst(range(0, N, step));
+  for (auto _ : state) { dst_v = src_v; }
+  state.SetBytesProcessed(state.iterations() * NBytes);
+  state.counters["processed"] = NBytes;
+  state.counters["step"]      = step;
+}
+BENCHMARK_TEMPLATE(Copy1DStrided, array_t<1>)->RangeMultiplier(8)->Range(KBmin, KBmax);
+BENCHMARK_TEMPLATE(Copy1DStrided, device_array_t<1>)->RangeMultiplier(8)->Range(KBmin, KBmax);
+
+template <typename DstArray, typename SrcArray>
+static void CopyBlockStrided(benchmark::State &state) {
+  long NBytesPerBlock = state.range(0) * 1024;
+  long step           = 2;
+  long n_blocks       = 10;
+  long N              = NBytesPerBlock / sizeof(value_t);
+  auto src            = SrcArray{nda::rand<value_t>(step * n_blocks, N)};
+  auto dst            = DstArray{nda::zeros<value_t>(step * n_blocks, N)};
+  auto src_v          = src(range(0, step * n_blocks, step), _);
+  auto dst_v          = dst(range(0, step * n_blocks, step), _);
+  for (auto _ : state) { dst_v = src_v; }
+  state.SetBytesProcessed(state.iterations() * NBytesPerBlock * n_blocks);
+  state.counters["processed"] = NBytesPerBlock * n_blocks;
+  state.counters["step"]      = step;
+  state.counters["n_blocks"]  = n_blocks;
+}
+BENCHMARK_TEMPLATE(CopyBlockStrided, array_t<2>, array_t<2>)->RangeMultiplier(8)->Range(KBmin, KBmax);
+BENCHMARK_TEMPLATE(CopyBlockStrided, device_array_t<2>, device_array_t<2>)->RangeMultiplier(8)->Range(KBmin, KBmax);
+BENCHMARK_TEMPLATE(CopyBlockStrided, array_t<2>, device_array_t<2>)->RangeMultiplier(8)->Range(KBmin, KBmax);
+BENCHMARK_TEMPLATE(CopyBlockStrided, device_array_t<2>, array_t<2>)->RangeMultiplier(8)->Range(KBmin, KBmax);
