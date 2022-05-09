6f0f46931fe49050af96c4b75620907640f51f75
Nils Wentzell <nwentzell@flatironinstitute.org>
2022-05-09 10:56:58 -0400

    Generalize nda_blas and nda_cublas test for various value_t and layout combinations

--- test/c++/nda_cublas.cpp
+++ test/c++/nda_cublas.cpp
@@ -44,52 +44,66 @@ TEST(CUBLAS, zgemmF) { test_gemm<dcomplex, F_layout>(); } //NOLINT
 
 // ==============================================================
 
-TEST(CUBLAS, gemv) { //NOLINT
+template <typename value_t, typename Layout>
+void test_gemv() {
 
-  nda::matrix<double, F_layout> A(5, 5);
+  nda::matrix<value_t, Layout> A(5, 5);
   A(i_, j_) << i_ + 2 * j_ + 1;
 
-  nda::vector<double> MC(5), MB(5);
+  nda::vector<value_t> MC(5), MB(5);
   MC() = 1;
   MB() = 0;
 
-  nda::cumatrix<double, F_layout> A_d{A};
-  nda::cuvector<double> MC_d{MC}, MB_d{MB};
+  nda::cumatrix<value_t, Layout> A_d{A};
+  nda::cuvector<value_t> MC_d{MC}, MB_d{MB};
 
   nda::range R(1, 3);
   nda::blas::gemv(1, A_d(R, R), MC_d(R), 0, MB_d(R));
   MB = MB_d;
-  EXPECT_ARRAY_NEAR(MB, nda::array<double, 1>{0, 10, 12, 0, 0});
+  EXPECT_ARRAY_NEAR(MB, nda::vector<value_t>{0, 10, 12, 0, 0});
 
-  nda::cumatrix_view<double> AT_d = transpose(A_d);
+  auto AT_d = transpose(A_d);
   nda::blas::gemv(1, AT_d(R, R), MC_d(R), 0, MB_d(R));
   MB = MB_d;
-  EXPECT_ARRAY_NEAR(MB, nda::vector<double>{0, 9, 13, 0, 0});
+  EXPECT_ARRAY_NEAR(MB, nda::vector<value_t>{0, 9, 13, 0, 0});
 
   // test operator*
   MB_d(R) = AT_d(R, R) * MC_d(R);
   MB()    = -8;
   MB(R)   = MB_d(R);
-  EXPECT_ARRAY_NEAR(MB, nda::vector<double>{-8, 9, 13, -8, -8});
+  EXPECT_ARRAY_NEAR(MB, nda::vector<value_t>{-8, 9, 13, -8, -8});
 }
 
+TEST(CUBLAS, gemv) { test_gemv<double, C_layout>(); }     //NOLINT
+TEST(CUBLAS, gemvF) { test_gemv<double, F_layout>(); }    //NOLINT
+TEST(CUBLAS, zgemv) { test_gemv<dcomplex, C_layout>(); }  //NOLINT
+TEST(CUBLAS, zgemvF) { test_gemv<dcomplex, F_layout>(); } //NOLINT
+
 //----------------------------
-TEST(CUBLAS, ger) { //NOLINT
 
-  nda::matrix<double, F_layout> M(2, 2);
+template <typename value_t, typename Layout>
+void test_ger() {
+
+  nda::matrix<value_t, Layout> M(2, 2);
   M = 0;
-  nda::array<double, 1> V{1, 2};
+  nda::array<value_t, 1> V{1, 2};
 
-  nda::cumatrix<double, F_layout> M_d{M};
-  nda::cuvector<double> V_d{V};
+  nda::cumatrix<value_t, Layout> M_d{M};
+  nda::cuvector<value_t> V_d{V};
 
   nda::blas::ger(1.0, V_d, V_d, M_d);
 
   M = M_d;
-  EXPECT_ARRAY_NEAR(M, nda::matrix<double>{{1, 2}, {2, 4}});
+  EXPECT_ARRAY_NEAR(M, nda::matrix<value_t>{{1, 2}, {2, 4}});
 }
 
+TEST(CUBLAS, dger) { test_ger<double, C_layout>(); }    //NOLINT
+TEST(CUBLAS, dgerF) { test_ger<double, F_layout>(); }   //NOLINT
+TEST(CUBLAS, zger) { test_ger<dcomplex, C_layout>(); }  //NOLINT
+TEST(CUBLAS, zgerF) { test_ger<dcomplex, C_layout>(); } //NOLINT
+
 //----------------------------
+
 TEST(CUBLAS, outer_product) { //NOLINT
 
   auto N = nda::rand<double>(2, 3);
@@ -107,24 +121,39 @@ TEST(CUBLAS, outer_product) { //NOLINT
 }
 
 //----------------------------
-TEST(CUBLAS, dot) { //NOLINT
 
-  nda::vector<double> a{1, 2, 3, 4, 5};
-  nda::vector<double> b{10, 20, 30, 40, 50};
+template <typename value_t>
+void test_dot() { //NOLINT
 
-  nda::cuvector<double> a_d{a}, b_d{b};
-  EXPECT_NEAR((nda::blas::dot(a_d, b_d)), (10 + 2 * 20 + 3 * 30 + 4 * 40 + 5 * 50), 1.e-14);
-  EXPECT_COMPLEX_NEAR((nda::blas::dotc(a_d, b_d)), (10 + 2 * 20 + 3 * 30 + 4 * 40 + 5 * 50), 1.e-14);
+  nda::vector<value_t> a{1, 2, 3, 4, 5};
+  nda::vector<value_t> b{10, 20, 30, 40, 50};
+  if constexpr (nda::is_complex_v<value_t>) {
+    a *= 1 + 1i;
+    b *= 1 + 2i;
+  }
+
+  nda::cuvector<value_t> a_d{a}, b_d{b};
+  EXPECT_COMPLEX_NEAR((nda::blas::dot(a_d, b_d)), (nda::blas::dot_generic(a, b)), 1.e-14);
 }
+
+TEST(CUBLAS, ddot) { test_dot<double>(); }   //NOLINT
+TEST(CUBLAS, zdot) { test_dot<dcomplex>(); } //NOLINT
 
 //----------------------------
-TEST(CUBLAS, dotc1) { //NOLINT
 
-  nda::vector<dcomplex> a{1, 2, 3};
-  nda::vector<dcomplex> b{10, 20, 30};
-  a *= 1 + 1i;
-  b *= 1 + 2i;
+template <typename value_t>
+void test_dotc() { //NOLINT
 
-  nda::cuvector<dcomplex> a_d{a}, b_d{b};
-  EXPECT_COMPLEX_NEAR((nda::blas::dotc(a_d, b_d)), (1 - 1i) * (1 + 2i) * (10 + 2 * 20 + 3 * 30), 1.e-14);
+  nda::vector<value_t> a{1, 2, 3, 4, 5};
+  nda::vector<value_t> b{10, 20, 30, 40, 50};
+  if constexpr (nda::is_complex_v<value_t>) {
+    a *= 1 + 1i;
+    b *= 1 + 2i;
+  }
+
+  nda::cuvector<value_t> a_d{a}, b_d{b};
+  EXPECT_COMPLEX_NEAR((nda::blas::dotc(a_d, b_d)), (nda::blas::dotc_generic(a, b)), 1.e-14);
 }
+
+TEST(CUBLAS, ddotc) { test_dotc<double>(); }   //NOLINT
+TEST(CUBLAS, zdotc) { test_dotc<dcomplex>(); } //NOLINT
