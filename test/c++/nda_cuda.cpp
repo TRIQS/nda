667cb7899ad037c478b19b0149b95d1a6d66b388
mmorale3 <miguel.mmorales@gmail.com>
2023-01-09 09:54:00 -0500

    added compile time unit test for new predicates in address_space

--- test/c++/nda_cuda.cpp
+++ test/c++/nda_cuda.cpp
@@ -25,10 +25,29 @@ template <size_t Rank>
 using cuarray_t = nda::cuarray<value_t, Rank>;
 
 template <size_t Rank>
+using cuarray_vt = nda::cuarray<value_t, Rank>;
+
+template <size_t Rank>
+using cuarray_cvt = nda::cuarray_const_view<value_t, Rank>;
+
+template <size_t Rank>
 using array_t = nda::array<value_t, Rank>;
 
 template <size_t Rank>
+using array_vt = nda::array_view<value_t, Rank>;
+
+template <size_t Rank>
 using array_cvt = nda::array_view<const value_t, Rank>;
+
+template <size_t Rank>
+using unmarray_t = basic_array<value_t, Rank, C_layout, 'A', heap<mem::Unified>>;
+
+template <size_t Rank>
+using unmarray_vt = basic_array_view<value_t, Rank, C_stride_layout, 'A', default_accessor, borrowed<mem::Unified>>;
+
+template <size_t Rank>
+using unmarray_cvt = basic_array_view<const value_t, Rank, C_stride_layout, 'A', default_accessor, borrowed<mem::Unified>>;
+
 
 TEST(Cuda, ConstructFromArray) { //NOLINT
   auto A = nda::rand<value_t>(N, N);
@@ -118,4 +137,42 @@ TEST(Cuda, Storage) { // NOLINT
   auto h2 = handle_heap<int>{h2_d};
 
   EXPECT_EQ(h2.data()[2], 89); //NOLINT
+}
+
+#include <nda/mem/address_space.hpp>
+TEST(Cuda, AddrSpace) { // NOLINT
+  // compile time checks
+  using namespace nda::mem;
+
+  static_assert(on_host<array_t<1>,array_vt<2>,array_cvt<3>>, "INTERNAL");
+  static_assert(not on_host<cuarray_t<1>,array_vt<2>,array_cvt<3>>, "INTERNAL");
+  static_assert(not on_host<array_t<1>,unmarray_vt<2>,array_cvt<3>>, "INTERNAL");
+
+  static_assert(on_device<cuarray_t<1>,cuarray_vt<2>,cuarray_cvt<3>>, "INTERNAL");
+  static_assert(not on_device<unmarray_t<1>,cuarray_vt<2>,cuarray_cvt<3>>, "INTERNAL");
+  static_assert(not on_device<cuarray_t<1>,array_vt<2>,cuarray_cvt<3>>, "INTERNAL");
+
+  static_assert(on_unified<unmarray_t<1>,unmarray_vt<2>,unmarray_cvt<3>>, "INTERNAL");
+  static_assert(not on_unified<array_t<1>,unmarray_vt<2>,unmarray_cvt<3>>, "INTERNAL");
+  static_assert(not on_unified<unmarray_t<1>,cuarray_vt<2>,unmarray_cvt<3>>, "INTERNAL");
+
+  static_assert(have_same_addr_space_v<array_t<1>,array_vt<2>,array_cvt<3>>, "INTERNAL");
+  static_assert(have_same_addr_space_v<cuarray_t<1>,cuarray_vt<2>,cuarray_cvt<3>>, "INTERNAL");
+  static_assert(have_same_addr_space_v<unmarray_t<1>,unmarray_vt<2>,unmarray_cvt<3>>, "INTERNAL");
+  static_assert(not have_same_addr_space_v<cuarray_t<1>,array_vt<2>,array_cvt<3>>, "INTERNAL");
+  static_assert(not have_same_addr_space_v<array_t<1>,cuarray_vt<2>,cuarray_cvt<3>>, "INTERNAL");
+  static_assert(not have_same_addr_space_v<array_t<1>,unmarray_vt<2>,unmarray_cvt<3>>, "INTERNAL");
+
+  static_assert(have_host_compatible_addr_space_v<array_t<1>,array_vt<2>,array_cvt<3>>, "INTERNAL");
+  static_assert(have_host_compatible_addr_space_v<array_t<1>,unmarray_vt<2>,unmarray_cvt<3>>, "INTERNAL");
+  static_assert(not have_host_compatible_addr_space_v<array_t<1>,unmarray_vt<2>,cuarray_cvt<3>>, "INTERNAL");
+
+  static_assert(have_device_compatible_addr_space_v<cuarray_t<1>,cuarray_vt<2>,cuarray_cvt<3>>, "INTERNAL");
+  static_assert(have_device_compatible_addr_space_v<cuarray_t<1>,unmarray_vt<2>,cuarray_cvt<3>>, "INTERNAL");
+  static_assert(not have_device_compatible_addr_space_v<cuarray_t<1>,unmarray_vt<2>,array_cvt<3>>, "INTERNAL");
+
+  static_assert(have_compatible_addr_space_v<cuarray_t<1>,unmarray_vt<2>,cuarray_cvt<3>>, "INTERNAL");
+  static_assert(have_compatible_addr_space_v<array_t<1>,unmarray_vt<2>,array_cvt<3>>, "INTERNAL");
+  static_assert(not have_compatible_addr_space_v<cuarray_t<1>,unmarray_vt<2>,array_cvt<3>>, "INTERNAL");
+
 }
