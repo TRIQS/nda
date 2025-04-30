d0ba0e998f9ed020f56af60e074c4903b113a85d
mmorale3 <miguel.mmorales@gmail.com>
2025-04-30 15:18:52 -0400

    added gerc

--- c++/nda/blas/interface/cxx_interface.hpp
+++ c++/nda/blas/interface/cxx_interface.hpp
@@ -53,6 +53,8 @@ namespace nda::blas::f77 {
 
   void ger(int M, int N, double alpha, const double *x, int incx, const double *Y, int incy, double *A, int LDA);
   void ger(int M, int N, dcomplex alpha, const dcomplex *x, int incx, const dcomplex *Y, int incy, dcomplex *A, int LDA);
+  void gerc(int M, int N, double alpha, const double *x, int incx, const double *Y, int incy, double *A, int LDA);
+  void gerc(int M, int N, dcomplex alpha, const dcomplex *x, int incx, const dcomplex *Y, int incy, dcomplex *A, int LDA);
 
   void scal(int M, double alpha, double *x, int incx);
   void scal(int M, dcomplex alpha, dcomplex *x, int incx);
