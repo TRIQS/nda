8b0d8a3237d8cb9f0d13ba900effe5476f5cea96
Thomas Hahn <thomas.hahn01@gmail.com>
2025-05-08 09:43:49 -0400

    Update blas/scal.hpp
    - do not allow conjugate expressions
    - update docs and tests

--- c++/nda/blas/scal.hpp
+++ c++/nda/blas/scal.hpp
@@ -13,9 +13,12 @@
 #include "./interface/cxx_interface.hpp"
 #include "./tools.hpp"
 #include "../concepts.hpp"
-#include "../device.hpp"
 #include "../mem/address_space.hpp"
 #include "../traits.hpp"
+
+#ifndef NDA_HAVE_DEVICE
+#include "../device.hpp"
+#endif // NDA_HAVE_DEVICE
 
 namespace nda::blas {
 
@@ -23,29 +26,24 @@ namespace nda::blas {
    * @ingroup linalg_blas
    * @brief Interface to the BLAS `scal` routine.
    *
-   * @details Scales a vector by a constant. This function calculates
-   * \f[
-   *   \mathbf{x} \leftarrow \alpha \mathbf{x} ;,
-   * \f]
+   * @details Scales a vector by a constant. This function calculates \f$ \mathbf{x} \leftarrow \alpha \mathbf{x} \f$,
    * where \f$ \alpha \f$ is a scalar constant and \f$ \mathbf{x} \f$ is a vector.
    *
-   * @tparam X nda::MemoryVector or a conjugate array expression.
-   * @param alpha Input scalar.
-   * @param x Input/Output vector to be scaled.
+   * @tparam X nda::MemoryVector type.
+   * @param alpha Input scalar \f$ \alpha \f$.
+   * @param x Input/Output vector \f$ \mathbf{x} \f$ to be scaled.
    */
-  template <typename X>
-    requires(MemoryVector<X> or is_conj_array_expr<X>)
+  template <MemoryVector X>
+    requires(is_blas_lapack_v<get_value_t<X>>)
   void scal(get_value_t<X> alpha, X &&x) { // NOLINT (temporary views are allowed here)
-    static_assert(is_blas_lapack_v<get_value_t<X>>, "Error in nda::blas::scal: Value type of vector is incompatible with blas");
-
-    if constexpr (mem::on_host<X>) {
-      f77::scal(x.size(), alpha, x.data(), x.indexmap().strides()[0]);
-    } else {
+    if constexpr (mem::have_device_compatible_addr_space<X>) {
 #if defined(NDA_HAVE_DEVICE)
       device::scal(x.size(), alpha, x.data(), x.indexmap().strides()[0]);
 #else
       compile_error_no_gpu();
 #endif
+    } else {
+      f77::scal(x.size(), alpha, x.data(), x.indexmap().strides()[0]);
     }
   }
 
