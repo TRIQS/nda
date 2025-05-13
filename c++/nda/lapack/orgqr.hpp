be5709a68a9b42104a89d026522ba12d91220b70
Thomas Hahn <thomas.hahn01@gmail.com>
2025-05-13 16:19:45 -0400

    Update lapack/geqp3.hpp, lapack/orgqr.hpp and lapack/ungqr.hpp
    - remove explicit throw statements --> always return the LAPACK info
    - consistent checks and function layout

--- c++/nda/lapack/orgqr.hpp
+++ c++/nda/lapack/orgqr.hpp
@@ -11,17 +11,15 @@
 #pragma once
 
 #include "./interface/cxx_interface.hpp"
+#include "../basic_array.hpp"
 #include "../concepts.hpp"
 #include "../declarations.hpp"
-#include "../exceptions.hpp"
-#include "../layout/policies.hpp"
 #include "../macros.hpp"
 #include "../mem/address_space.hpp"
-#include "../mem/policies.hpp"
 #include "../traits.hpp"
 
+#include <algorithm>
 #include <cmath>
-#include <complex>
 #include <type_traits>
 
 namespace nda::lapack {
@@ -30,47 +28,56 @@ namespace nda::lapack {
    * @ingroup linalg_lapack
    * @brief Interface to the LAPACK `orgqr` routine.
    *
-   * @details Generates an m-by-n real matrix \f$ \mathbf{Q} \f$ with orthonormal columns, which is defined as the first
-   * n columns of a product of k elementary reflectors of order m:
+   * @details Generates an \f$ m \times \min(m,n) = k \f$ real matrix \f$ \mathbf{Q} \f$ with orthonormal columns, which
+   * is defined as the first \f$ k \f$ columns of a product of \f$ k \f$ elementary reflectors of order \f$ m \f$
    * \f[
    *   \mathbf{Q} = \mathbf{H}(1) \mathbf{H}(2) \ldots \mathbf{H}(k) \; ,
    * \f]
-   * as returned by `geqrf`.
+   * as returned by nda::lapack::geqp3.
+   *
+   * Each \f$ \mathbf{H}(i) \f$ has the form
+   * \f[
+   *   \mathbf{H}(i) = \mathbf{I} - \tau_i * \mathbf{v}_i \mathbf{v}_i^H
+   * \f]
+   * where \f$ \tau_i \f$ is a real scalar, and \f$ \mathbf{v}_i \f$ is a real vector with
+   * - elements \f$ 1 \f$ to \f$ i - 1 \f$ equal to 0,
+   * - element \f$ i \f$ equal to 1 and
+   * - elements \f$ i + 1 \f$ to \f$ m \f$ stored in the elements \f$ i + 1 \f$ to \f$ m \f$ in column \f$ i \f$ of
+   * matrix \f$ \mathbf{A} \f$.
    *
    * @tparam A nda::MemoryMatrix with double value type.
    * @tparam TAU nda::MemoryVector with double value type.
-   * @param a Input/output matrix. On entry, the i-th column must contain the vector which defines the elementary
-   * reflector \f$ H(i) \; , i = 1,2,...,k \f$, as returned by `geqrf` in the first k columns. On exit, the m-by-n
-   * matrix \f$ \mathbf{Q} \f$.
-   * @param tau Input vector. `tau(i)` must contain the scalar factor of the elementary reflector \f$ \mathbf{H}(i) \f$,
-   * as returned by `geqrf`.
+   * @param a Input/output matrix. On entry, the i<sup>th</sup> column must contain the vector which defines the
+   * elementary reflector \f$ H(i) \; , i = 1,2,...,k \f$, as returned by nda::lapack::geqp3 in the first \f$ k \f$
+   * columns. On exit, the \f$ m \times \min(m,n) = k \f$ matrix \f$ \mathbf{Q} \f$.
+   * @param tau Input vector. \f$ \tau_i \f$ must contain the scalar factor of the elementary reflector \f$
+   * \mathbf{H}(i) \f$, as returned by nda::lapack::geqp3.
    * @return Integer return code from the LAPACK call.
    */
   template <MemoryMatrix A, MemoryVector TAU>
-    requires(mem::on_host<A> and std::is_same_v<double, get_value_t<A>> and have_same_value_type_v<A, TAU>
-             and mem::have_compatible_addr_space<A, TAU>)
+    requires(mem::have_host_compatible_addr_space<A> and std::is_same_v<double, get_value_t<A>> and have_same_value_type_v<A, TAU>)
   int orgqr(A &&a, TAU &&tau) { // NOLINT (temporary views are allowed here)
-    static_assert(has_F_layout<A>, "Error in nda::lapack::orgqr: C order is not supported");
-    static_assert(mem::have_host_compatible_addr_space<A, TAU>, "Error in nda::lapack::orgqr: Only CPU is supported");
+    static_assert(has_F_layout<A>, "Error in nda::lapack::orgqr: A must have Fortran layout");
 
-    // must be lapack compatible
+    // check the dimensions of the input/output arrays/views
+    auto const [m, n] = a.shape();
+    auto const k      = std::min(m, n);
+    EXPECTS(tau.size() == k);
+
+    // arrays/views must be LAPACK compatible
     EXPECTS(a.indexmap().min_stride() == 1);
     EXPECTS(tau.indexmap().min_stride() == 1);
 
-    // first call to get the optimal buffersize
-    using value_type = get_value_t<A>;
-    value_type bufferSize_T{};
-    auto [m, n] = a.shape();
-    auto k      = tau.size();
-    int info    = 0;
-    lapack::f77::orgqr(m, std::min(m, n), k, a.data(), get_ld(a), tau.data(), &bufferSize_T, -1, info);
-    int bufferSize = static_cast<int>(std::ceil(std::real(bufferSize_T)));
+    // first call to get the optimal buffer size
+    double tmp_lwork{};
+    int info = 0;
+    lapack::f77::orgqr(m, k, k, a.data(), get_ld(a), tau.data(), &tmp_lwork, -1, info);
+    int lwork = static_cast<int>(std::ceil(tmp_lwork));
 
     // allocate work buffer and perform actual library call
-    nda::array<value_type, 1, C_layout, heap<mem::get_addr_space<A>>> work(bufferSize);
-    lapack::f77::orgqr(m, std::min(m, n), k, a.data(), get_ld(a), tau.data(), work.data(), bufferSize, info);
+    array<double, 1> work(lwork);
+    lapack::f77::orgqr(m, k, k, a.data(), get_ld(a), tau.data(), work.data(), lwork, info);
 
-    if (info) NDA_RUNTIME_ERROR << "Error in nda::lapack::orgqr: info = " << info;
     return info;
   }
 
