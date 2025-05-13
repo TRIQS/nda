19a66d24afa22d67ffc3fd1914485c97a7f4159f
Thomas Hahn <thomas.hahn01@gmail.com>
2025-05-13 10:39:44 -0400

    Update blas/gemm.hpp and linalg/matmul.hpp
    - move blas::gemm_generic into linalg::detail namespace in linalg/matmul.hpp
    - linalg::matvecmul
      - allow non-contiguous matrices
      - move various helper lambdas into linalg::detail namespace

--- c++/nda/blas/gemm.hpp
+++ c++/nda/blas/gemm.hpp
@@ -20,7 +20,7 @@
 
 #ifndef NDA_HAVE_DEVICE
 #include "../device.hpp"
-#endif
+#endif // NDA_HAVE_DEVICE
 
 #include <tuple>
 #include <utility>
@@ -33,102 +33,61 @@ namespace nda::blas {
    */
 
   /**
-   * @brief Generic nda::blas::gemm implementation for types not supported by BLAS/LAPACK.
-   *
-   * @tparam A Some matrix type.
-   * @tparam B Some matrix type.
-   * @tparam C Some matrix type.
-   * @param alpha Input scalar.
-   * @param a Input matrix of size m-by-k.
-   * @param b Input matrix of size k-by-n.
-   * @param beta Input scalar.
-   * @param c Input/Output matrix of size m-by-n.
-   */
-  template <Matrix A, Matrix B, MemoryMatrix C>
-  void gemm_generic(typename A::value_type alpha, A const &a, B const &b, typename A::value_type beta,
-                    C &&c) { // NOLINT (temporary views are allowed here)
-    EXPECTS(a.extent(1) == b.extent(0));
-    EXPECTS(a.extent(0) == c.extent(0));
-    EXPECTS(b.extent(1) == c.extent(1));
-
-    if (beta == 0.0) {
-      c = 0 * alpha;
-    } else {
-      c *= beta;
-    }
-
-    for (int i = 0; i < a.extent(0); ++i) {
-      for (int j = 0; j < b.extent(1); ++j) {
-        for (int k = 0; k < a.extent(1); ++k) c(i, j) += alpha * a(i, k) * b(k, j);
-      }
-    }
-  }
-
-  /**
    * @brief Interface to the BLAS `gemm` routine.
    *
    * @details This function performs one of the matrix-matrix operations
    * \f[
-   *   \mathbf{C} \leftarrow \alpha \mathrm{op}(\mathbf{A}) \mathrm{op}(\mathbf{B}) + \beta \mathbf{C} \;,
+   *   \mathbf{C} \leftarrow \alpha \mathrm{op}_A(\mathbf{A}) \mathrm{op}_B(\mathbf{B}) + \beta \mathbf{C} \;,
    * \f]
    * where \f$ \mathrm{op}(\mathbf{X}) \f$ is one of
+   * - \f$ \mathrm{op}(\mathbf{X}) = \mathbf{X} \f$ or,
+   * - \f$ \mathrm{op}(\mathbf{X}) = \mathbf{X}^* \f$ (only if \f$ \mathbf{X} \f$ is in C-layout).
    *
-   * - \f$ \mathrm{op}(\mathbf{X}) = \mathbf{X} \f$,
-   * - \f$ \mathrm{op}(\mathbf{X}) = \mathbf{X}^T \f$ or
-   * - \f$ \mathrm{op}(\mathbf{X}) = \mathbf{X}^H \f$.
+   * Here, \f$ \alpha \f$ and \f$ \beta \f$ are scalars, and \f$ \mathbf{A} \f$, \f$ \mathbf{B} \f$ and \f$ \mathbf{C}
+   * \f$ are matrices of size \f$ m \times k \f$, \f$ k \times n \f$ and \f$ m \times n \f$, respectively.
    *
-   * Here, \f$ \alpha \f$ and \f$ \beta \f$ are scalars, and \f$ \mathbf{A} \f$, \f$ \mathbf{B} \f$ are matrices with
-   * \f$ \mathrm{op}(\mathbf{A}) \f$ is an m-by-k matrix, \f$ \mathrm{op}(\mathbf{B}) \f$ is a k-by-n matrix and
-   * \f$ \mathrm{op}(\mathbf{C}) \f$ is an m-by-n matrix.
+   * @note If matrix \f$ \mathbf{C} \f$ is in C-layout, we transpose both \f$ \mathbf{A} \f$ and \f$ \mathbf{B} \f$ and
+   * swap their order.
    *
    * @tparam A nda::Matrix type.
    * @tparam B nda::Matrix type.
    * @tparam C nda::MemoryMatrix type.
-   * @param alpha Input scalar.
-   * @param a Input matrix of size m-by-k.
-   * @param b Input matrix of size k-by-n.
-   * @param beta Input scalar.
-   * @param c Input/Output matrix of size m-by-n.
+   * @param alpha Input scalar \f$ \alpha \f$.
+   * @param a Input matrix \f$ \mathrm{op}_A(\mathbf{A}) \f$ of size \f$ m \times k \f$.
+   * @param b Input matrix \f$ \mathrm{op}_B(\mathbf{B}) \f$ of size \f$ k \times n \f$.
+   * @param beta Input scalar \f$ beta \f$.
+   * @param c Input/Output matrix \f$ \mathbf{C} \f$ of size \f$ m \times n \f$.
    */
   template <Matrix A, Matrix B, MemoryMatrix C>
     requires((MemoryMatrix<A> or is_conj_array_expr<A>) and (MemoryMatrix<B> or is_conj_array_expr<B>)
-             and have_same_value_type_v<A, B, C> and is_blas_lapack_v<get_value_t<A>>)
+             and have_same_value_type_v<A, B, C> and mem::have_compatible_addr_space<A, B, C> and is_blas_lapack_v<get_value_t<A>>)
   void gemm(get_value_t<A> alpha, A const &a, B const &b, get_value_t<A> beta, C &&c) {
-    // get underlying matrix in case it is given as a lazy expression
-    auto to_mat = []<typename Z>(Z const &z) -> auto & {
-      if constexpr (is_conj_array_expr<Z>)
-        return std::get<0>(z.a);
-      else
-        return z;
-    };
-    auto &mat_a = to_mat(a);
-    auto &mat_b = to_mat(b);
-
-    // compile-time checks
-    using mat_a_type = decltype(mat_a);
-    using mat_b_type = decltype(mat_b);
-    static_assert(mem::have_compatible_addr_space<mat_a_type, mat_b_type, C>, "Error in nda::blas::gemm: Incompatible memory address spaces");
-
-    // runtime checks
-    EXPECTS(mat_a.extent(1) == mat_b.extent(0));
-    EXPECTS(mat_a.extent(0) == c.extent(0));
-    EXPECTS(mat_b.extent(1) == c.extent(1));
-    EXPECTS(mat_a.indexmap().min_stride() == 1);
-    EXPECTS(mat_b.indexmap().min_stride() == 1);
-    EXPECTS(c.indexmap().min_stride() == 1);
-
-    // c is in C order: compute the transpose of the product in Fortran order
+    // if C is in C-layout, compute the transpose of the product in Fortran order
     if constexpr (has_C_layout<C>) {
       gemm(alpha, transpose(b), transpose(a), beta, transpose(std::forward<C>(c)));
-    } else { // c is in Fortran order
-      static constexpr bool conj_A = is_conj_array_expr<A>;
-      static constexpr bool conj_B = is_conj_array_expr<B>;
-      char op_a                    = get_op<conj_A, /* transpose = */ has_C_layout<mat_a_type>>;
-      char op_b                    = get_op<conj_B, /* transpose = */ has_C_layout<mat_b_type>>;
-      auto [m, k]                  = mat_a.shape();
-      auto n                       = mat_b.extent(1);
+    } else {
+      // get underlying matrix in case it is given as a conjugate expression
+      auto &mat_a = get_array(a);
+      auto &mat_b = get_array(b);
 
-      if constexpr (mem::have_device_compatible_addr_space<mat_a_type, mat_b_type, C>) {
+      // check the dimensions of the input/output arrays/views
+      auto const [m, k] = mat_a.shape();
+      auto const [l, n] = mat_b.shape();
+      EXPECTS(k == l);
+      EXPECTS(m == c.extent(0));
+      EXPECTS(n == c.extent(1));
+
+      // arrays/views must be BLAS compatible
+      EXPECTS(mat_a.indexmap().min_stride() == 1);
+      EXPECTS(mat_b.indexmap().min_stride() == 1);
+      EXPECTS(c.indexmap().min_stride() == 1);
+
+      // check for conjugate lazy expressions and C-layouts
+      char op_a = get_op<is_conj_array_expr<A>, has_C_layout<A>>;
+      char op_b = get_op<is_conj_array_expr<B>, has_C_layout<B>>;
+
+      // perform the actual library call
+      if constexpr (mem::have_device_compatible_addr_space<A, B, C>) {
 #if defined(NDA_HAVE_DEVICE)
         device::gemm(op_a, op_b, m, n, k, alpha, mat_a.data(), get_ld(mat_a), mat_b.data(), get_ld(mat_b), beta, c.data(), get_ld(c));
 #else
--- c++/nda/linalg/matmul.hpp
+++ c++/nda/linalg/matmul.hpp
@@ -5,16 +5,18 @@
 
 /**
  * @file
- * @brief Provides matrix-matrix an matrix-vector multiplication.
+ * @brief Provides a generic matrix-matrix multiplication.
  */
 
 #pragma once
 
+#include "../basic_array.hpp"
 #include "../basic_functions.hpp"
 #include "../blas/gemm.hpp"
 #include "../blas/tools.hpp"
 #include "../concepts.hpp"
 #include "../declarations.hpp"
+#include "../exceptions.hpp"
 #include "../layout/policies.hpp"
 #include "../mem/address_space.hpp"
 #include "../mem/policies.hpp"
@@ -23,7 +25,7 @@
 #include <type_traits>
 #include <utility>
 
-namespace nda {
+namespace nda::linalg {
 
   /**
    * @addtogroup linalg_tools
@@ -32,96 +34,130 @@ namespace nda {
 
   namespace detail {
 
-    // Helper variable template to check if the three matrix types can be passed to gemm.
-    // The following combinations are allowed (gemm can only be called with 'N', 'T' or 'C' op tags):
-    // - C in Fortran layout:
-    // -- A/B is not a conj expression and has Fortran layout
-    // -- A/B is a conj expression and has C layout
-    // - C in C layout:
-    // -- A/B is not a conj expression and has C layout
-    // -- A/B is a conj expression and has Fortran layout
-    template <Matrix A, Matrix B, MemoryMatrix C, bool conj_A = blas::is_conj_array_expr<A>, bool conj_B = blas::is_conj_array_expr<B>>
-      requires((MemoryMatrix<A> or conj_A) and (MemoryMatrix<B> or conj_B))
-    static constexpr bool is_valid_gemm_triple = []() {
-      using blas::has_F_layout;
-      if constexpr (has_F_layout<C>) {
-        return !(conj_A and has_F_layout<A>) and !(conj_B and has_F_layout<B>);
-      } else {
-        return !(conj_B and !has_F_layout<B>) and !(conj_A and !has_F_layout<A>);
+    /// Generic matrix-matrix multiplication for types not supported by BLAS.
+    template <Matrix A, Matrix B, MemoryMatrix C>
+      requires(mem::have_host_compatible_addr_space<A, B, C>)
+    void gemm_generic(auto alpha, A const &a, B const &b, auto beta, C &&c) { // NOLINT (temporary views are allowed here)
+      // check the dimensions of the input/output arrays/views
+      auto const [m, k] = a.shape();
+      auto const [l, n] = b.shape();
+      EXPECTS(k == l);
+      EXPECTS(m == c.extent(0));
+      EXPECTS(n == c.extent(1));
+
+      // perform the matrix-matrix multiplication
+      for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+          c(i, j) = beta * c(i, j);
+          for (int r = 0; r < k; ++r) c(i, j) += alpha * a(i, r) * b(r, j);
+        }
       }
-    }();
+    }
+
+    // Make compile time checks if blas::gemm can handle the given input matrix. If it can, simply forward the matrix.
+    // Otherwise, return a copy with the given value type T, layout policy LP and container policy CP.
+    template <typename T, typename LP, typename CP, MemoryMatrix C, Matrix A>
+    decltype(auto) get_gemm_matrix(A &&a) {
+      if constexpr (requires { blas::get_array(a); } and std::is_same_v<get_value_t<A>, T>) {
+        if constexpr (MemoryMatrix<A>
+                      or (blas::is_conj_array_expr<A>
+                          and ((blas::has_F_layout<C> and blas::has_C_layout<A>) or (blas::has_C_layout<C> and blas::has_F_layout<A>)))) {
+          return std::forward<A>(a);
+        } else {
+          return matrix<T, LP, CP>{a};
+        }
+      } else {
+        return matrix<T, LP, CP>{a};
+      }
+    }
+
+    // Make the call to nda::blas::gemm (with copies of the matrices if they are not contiguous).
+    template <Matrix A, Matrix B, MemoryMatrix C>
+    void make_gemm_call(A const &a, B const &b, C &c) {
+      auto try_gemm = []<typename A2, typename B2, typename C2>(A2 &&a2, B2 &&b2, C2 &&c2) {
+        if constexpr (requires { blas::gemm(1, std::forward<A2>(a2), std::forward<B2>(b2), 0, std::forward<C2>(c2)); }) {
+          blas::gemm(1, std::forward<A2>(a2), std::forward<B2>(b2), 0, std::forward<C2>(c2));
+        } else {
+          NDA_RUNTIME_ERROR << "Error in nda::linalg::matmul: Cannot call blas::gemm with the given input arrys/views.";
+        }
+      };
+      if (blas::get_array(a).is_contiguous()) {
+        if (blas::get_array(b).is_contiguous()) {
+          try_gemm(a, b, c);
+        } else {
+          try_gemm(a, nda::make_regular(b), c);
+        }
+      } else {
+        if (blas::get_array(b).is_contiguous()) {
+          try_gemm(nda::make_regular(a), b, c);
+        } else {
+          try_gemm(nda::make_regular(a), nda::make_regular(b), c);
+        }
+      }
+    }
 
     // Get the layout policy for a given array type.
     template <Array A>
-    using get_layout_policy = typename std::remove_reference_t<decltype(make_regular(std::declval<A>()))>::layout_policy_t;
+    using get_layout_policy = typename std::remove_cvref_t<decltype(make_regular(std::declval<A>()))>::layout_policy_t;
 
   } // namespace detail
 
   /**
-   * @brief Perform a matrix-matrix multiplication.
+   * @brief Compute the matrix-matrix product of two nda::matrix objects.
    *
-   * @details It is generic in the sense that it allows the input matrices to belong to a different
-   * nda::mem::AddressSpace (as long as they are compatible).
+   * @details This function computes the matrix-matrix product \f$ \mathrm{op}_1(\mathbf{A}) \mathrm{op}_2(\mathbf{B})
+   * \f$, where \f$ \mathbf{A} \f$ and \f$ \mathbf{B} \f$ are \f$ m \times k \f$ and \f$ k \times n \f$ matrices
+   * respectively, \f$ \mathrm{op}_i \f$ can be some lazy operation, e.g. nda::conj, nda::sin, etc.
    *
-   * If possible, it uses nda::blas::gemm, otherwise it calls nda::blas::gemm_generic.
+   * We try to call nda::blas::gemm whenever possible, i.e. when the value type of the result is compatible with
+   * nda::is_blas_lapack_v, even if this requires to make copies of the input arrays/views. Otherwise, we perform a very
+   * naive and inefficient matrix-matrix multiplication manually.
    *
-   * @tparam A nda::Matrix type of lhs operand.
-   * @tparam B nda::Matrix type of rhs operand.
-   * @param a Left hand side matrix operand.
-   * @param b Right hand side matrix operand.
-   * @return Result of the matrix-matrix multiplication.
+   * Therefore, if performance is important, users should make sure to pass input arrays/views which are compatible with
+   * nda::blas::gemm.
+   *
+   * @note The layout of the returned matrix depends on the layout of the input matrices. If both input matrices are in
+   * Fortran-layout, the returned matrix is also in Fortran layout. Otherwise, it is in C-layout.
+   * 
+   * @warning This function might make copies of the input arrays/views. When working on the device memory space, this 
+   * may lead to runtime errors if the copying fails.
+   *
+   * @tparam A nda::Matrix type.
+   * @tparam B nda::Matrix type.
+   * @param a Input matrix \f$ \mathbf{A} \f$ of size \f$ m \times k \f$.
+   * @param a Input matrix \f$ \mathbf{B} \f$ of size \f$ k \times n \f$.
+   * @return Resulting matrix of the matrix-matrix multiplication of size \f$ m \times n \f$.
    */
   template <Matrix A, Matrix B>
   auto matmul(A &&a, B &&b) { // NOLINT (temporary views are allowed here)
-    // check dimensions
-    EXPECTS_WITH_MESSAGE(a.shape()[1] == b.shape()[0], "Error in nda::matmul: Dimension mismatch in matrix-matrix product");
+    // get the return type
+    using value_t    = decltype(a(0, 0) * b(0, 0));
+    using cont_pol   = heap<mem::common_addr_space<A, B>>;
+    using layout_pol = std::conditional_t<get_layout_info<A>.stride_order == get_layout_info<B>.stride_order, detail::get_layout_policy<A>, C_layout>;
+    using return_t   = matrix<value_t, layout_pol, cont_pol>;
 
-    // check address space compatibility
-    static constexpr auto L_adr_spc = mem::get_addr_space<A>;
-    static constexpr auto R_adr_spc = mem::get_addr_space<B>;
-    mem::check_adr_sp_valid<L_adr_spc, R_adr_spc>();
-
-    // get resulting value type, layout policy and matrix type
-    using value_t = decltype(get_value_t<A>{} * get_value_t<B>{});
-    using layout_policy =
-       std::conditional_t<get_layout_info<A>.stride_order == get_layout_info<B>.stride_order, detail::get_layout_policy<A>, C_layout>;
-    using matrix_t = basic_array<value_t, 2, layout_policy, 'M', nda::heap<mem::combine<L_adr_spc, R_adr_spc>>>;
-
-    // perform matrix-matrix multiplication
-    auto result = matrix_t(a.shape()[0], b.shape()[1]);
-    if constexpr (is_blas_lapack_v<value_t>) {
-      // for double or complex value types we use blas::gemm
-      // lambda to form a new matrix with the correct value type if necessary
-      auto as_container = []<Matrix M>(M &&m) -> decltype(auto) {
-        if constexpr (std::is_same_v<get_value_t<M>, value_t> and (MemoryMatrix<M> or blas::is_conj_array_expr<M>))
-          return std::forward<M>(m);
-        else
-          return matrix_t{std::forward<M>(m)};
-      };
-
-      // MSAN has no way to know that we are calling with beta = 0, hence this is not necessary.
-      // Of course, in production code, we do NOT waste time to do this.
+    // result matrix (MSAN complains if it is not initialized)
+    auto res = return_t(a.shape()[0], b.shape()[1]);
 #if defined(__has_feature)
 #if __has_feature(memory_sanitizer)
-      result = 0;
+    res = 0;
 #endif
 #endif
 
-      // check if we can call gemm directly
-      if constexpr (detail::is_valid_gemm_triple<decltype(as_container(a)), decltype(as_container(b)), matrix_t>) {
-        blas::gemm(1, as_container(a), as_container(b), 0, result);
-      } else {
-        // otherwise, turn the lhs and rhs first into regular matrices and then call gemm
-        blas::gemm(1, make_regular(as_container(a)), make_regular(as_container(b)), 0, result);
-      }
+    // perform matrix-matrix multiplication (if possible we try to call blas::gemv even if this requires making copies)
+    if constexpr (is_blas_lapack_v<value_t>) {
+      // check at compile time if we need to make a copy of the input matrices
+      auto &&a_mat = detail::get_gemm_matrix<value_t, layout_pol, cont_pol, return_t>(a);
+      auto &&b_mat = detail::get_gemm_matrix<value_t, layout_pol, cont_pol, return_t>(b);
 
+      // check at runtime if the input matrices are contiguous, make copies if not and call blas::gemm
+      detail::make_gemm_call(a_mat, b_mat, res);
     } else {
-      // for other value types we use a generic implementation
-      blas::gemm_generic(1, a, b, 0, result);
+      detail::gemm_generic(1, a, b, 0, res);
     }
-    return result;
+    return res;
   }
 
   /** @} */
 
-} // namespace nda
+} // namespace nda::linalg
