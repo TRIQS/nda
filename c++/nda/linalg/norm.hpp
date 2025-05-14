8ec5504881470857064a5baadfbb65addc717e39
Thomas Hahn <thomas.hahn01@gmail.com>
2025-05-14 15:08:57 -0400

    Update linalg/norm.hpp
    - move norm into linalg namespace

--- c++/nda/linalg/norm.hpp
+++ c++/nda/linalg/norm.hpp
@@ -3,8 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // See LICENSE in the root of this distribution for details.
 
-#pragma once
-
 /**
  * @file
  * @brief Provides the p-norm for general arrays/views of rank 1 and with scalar elements.
@@ -23,30 +21,29 @@
 #include <complex>
 #include <limits>
 
-namespace nda {
+namespace nda::linalg {
 
   /**
    * @ingroup linalg_tools
-   * @brief Calculate the p-norm of an nda::ArrayOfRank<1> object \f$ \mathbf{x} \f$ with scalar values.
-   * The p-norm is defined as
+   * @brief Calculate the p-norm of an nda::ArrayOfRank<1> object \f$ \mathbf{x} \f$ with scalar values. The p-norm is
+   * defined as
    * \f[
    *   || \mathbf{x} ||_p = \left( \sum_{i=0}^{N-1} |x_i|^p \right)^{1/p}
    * \f]
-   * with the special cases (following numpy.linalg.norm convention)
+   * with the special cases (following `numpy.linalg.norm` convention)
    *
    * - \f$ || \mathbf{x} ||_0 = \text{number of non-zero elements} \f$,
-   * - \f$ || \mathbf{x} ||_{\infty} = \max_i |x_i| \f$,
-   * - \f$ || \mathbf{x} ||_{-\infty} = \min_i |x_i| \f$.
+   * - \f$ || \mathbf{x} ||_{\infty} = \max \{ |x_i| : i = 0, \dots, N - 1 \} \f$,
+   * - \f$ || \mathbf{x} ||_{-\infty} = \min \{ |x_i| : i = 0, \dots, N - 1 \} \f$.
    *
    * @tparam A nda::ArrayOfRank<1> type.
-   * @param a nda::ArrayOfRank<1> object.
+   * @param a nda::ArrayOfRank<1> object \f$ \mathbf{x} \f$.
    * @param p Order of the norm.
-   * @return Norm of the array/view as a double.
+   * @return p-norm of the array/view.
    */
   template <ArrayOfRank<1> A>
+    requires(Scalar<get_value_t<A>>)
   double norm(A const &a, double p = 2.0) {
-    static_assert(Scalar<get_value_t<A>>, "Error in nda::norm: Only scalar value types are allowed");
-
     if (p == 2.0) [[likely]] {
       if constexpr (MemoryArray<A>)
         return std::sqrt(std::real(nda::blas::dotc(a, a)));
@@ -69,4 +66,4 @@ namespace nda {
     }
   }
 
-} // namespace nda
+} // namespace nda::linalg
