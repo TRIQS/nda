4d6fc8462e1b66320ab63b604245eb080f3bdeec
Thomas Hahn <thomas.hahn01@gmail.com>
2025-05-15 19:02:30 -0400

    Update linalg/det.hpp
    - rename determinant to det, determinant_in_place to det_in_place
    - introduce det_1d, det_2d and det_3d
    - only allow matrices on host compatible address spaces and with double or complex value types
    - call optimized routines for matrices with less than 4 rows/columns

--- c++/nda/linalg/det.hpp
+++ c++/nda/linalg/det.hpp
@@ -12,23 +12,15 @@
 
 #include "../basic_array.hpp"
 #include "../basic_functions.hpp"
-#include "../clef/make_lazy.hpp"
 #include "../concepts.hpp"
 #include "../exceptions.hpp"
 #include "../lapack/getrf.hpp"
-#include "../lapack/getri.hpp"
-#include "../layout/policies.hpp"
 #include "../matrix_functions.hpp"
 #include "../mem/address_space.hpp"
 #include "../mem/policies.hpp"
-#include "../print.hpp"
 #include "../traits.hpp"
 
-#include <iostream>
-#include <type_traits>
-#include <utility>
-
-namespace nda {
+namespace nda::linalg {
 
   /**
    * @addtogroup linalg_tools
@@ -36,65 +28,117 @@ namespace nda {
    */
 
   /**
-   * @brief Compute the determinant of a square matrix/view.
+   * @brief Compute the determinant of a \f$ 1 \times 1 \f$ matrix \f$ \mathbf{M} \f$.
    *
-   * @details It uses nda::lapack::getrf to compute the LU decomposition of the matrix and then calculates the
-   * determinant by multiplying the diagonal elements of the \f$ \mathbf{U} \f$ matrix and taking into account that
-   * `getrf` may change the ordering of the rows/columns of the matrix.
-   *
-   * The given matrix/view is modified in place.
-   *
-   * @tparam M Type of the matrix/view.
-   * @param m Matrix/view object.
-   * @return Determinant of the matrix/view.
+   * @tparam M nda::Matrix type.
+   * @param m Input matrix. The matrix \f$ \mathbf{M} \f$.
+   * @return \f$ \det(\mathbf{M}) \f$.
    */
-  template <typename M>
-  auto determinant_in_place(M &m)
-    requires(is_matrix_or_view_v<M>)
-  {
-    using value_t = get_value_t<M>;
-    static_assert(std::is_convertible_v<value_t, double> or std::is_convertible_v<value_t, std::complex<double>>,
-                  "Error in nda::determinant_in_place: Value type needs to be convertible to double or std::complex<double>");
-    static_assert(not std::is_const_v<M>, "Error in nda::determinant_in_place: Value type cannot be const");
-
-    // special case for an empty matrix
-    if (m.empty()) return value_t{1};
-
-    // check if the matrix is square
-    if (m.extent(0) != m.extent(1)) NDA_RUNTIME_ERROR << "Error in nda::determinant_in_place: Matrix is not square: " << m.shape();
-
-    // calculate the LU decomposition using lapack getrf
-    const int dim = m.extent(0);
-    basic_array<int, 1, C_layout, 'A', sso<100>> ipiv(dim);
-    int info = lapack::getrf(m, ipiv); // it is ok to be in C order
-    if (info < 0) NDA_RUNTIME_ERROR << "Error in nda::determinant_in_place: info = " << info;
-
-    // calculate the determinant from the LU decomposition
-    auto det    = value_t{1};
-    int n_flips = 0;
-    for (int i = 0; i < dim; i++) {
-      det *= m(i, i);
-      // count the number of column interchanges performed by getrf
-      if (ipiv(i) != i + 1) ++n_flips;
-    }
-
-    return ((n_flips % 2 == 1) ? -det : det);
+  template <Matrix M>
+    requires(get_algebra<M> == 'M' and nda::mem::have_host_compatible_addr_space<M> and is_blas_lapack_v<get_value_t<M>>)
+  auto det_1d(M const &m) {
+    EXPECTS(is_matrix_square(m) and m.shape()[0] == 1);
+    return m(0, 0);
   }
 
   /**
-   * @brief Compute the determinant of a square matrix/view.
+   * @brief Compute the determinant of a \f$ 2 \times 2 \f$ matrix \f$ \mathbf{M} \f$.
    *
-   * @details The given matrix/view is not modified. It first makes a copy of the given matrix/view and then calls
-   * nda::determinant_in_place with the copy.
-   *
-   * @tparam M Type of the matrix/view.
-   * @param m Matrix/view object.
-   * @return Determinant of the matrix/view.
+   * @tparam M nda::Matrix type.
+   * @param m Input matrix. The matrix \f$ \mathbf{M} \f$.
+   * @return \f$ \det(\mathbf{M}) \f$.
    */
-  template <typename M>
-  auto determinant(M const &m) {
+  template <Matrix M>
+    requires(get_algebra<M> == 'M' and nda::mem::have_host_compatible_addr_space<M> and is_blas_lapack_v<get_value_t<M>>)
+  auto det_2d(M const &m) {
+    EXPECTS(is_matrix_square(m) and m.shape()[0] == 2);
+    return m(0, 0) * m(1, 1) - m(0, 1) * m(1, 0);
+  }
+
+  /**
+   * @brief Compute the inverse of a \f$ 3 \times 3 \f$ matrix \f$ \mathbf{M} \f$.
+   *
+   * @tparam M nda::Matrix type.
+   * @param m Input matrix. The matrix \f$ \mathbf{M} \f$.
+   * @return \f$ \det(\mathbf{M}) \f$.
+   */
+  template <Matrix M>
+    requires(get_algebra<M> == 'M' and nda::mem::have_host_compatible_addr_space<M> and is_blas_lapack_v<get_value_t<M>>)
+  auto det_3d(M const &m) {
+    EXPECTS(is_matrix_square(m) and m.shape()[0] == 3);
+    return m(0, 0) * (m(1, 1) * m(2, 2) - m(1, 2) * m(2, 1)) - m(0, 1) * (m(1, 0) * m(2, 2) - m(1, 2) * m(2, 0))
+       + m(0, 2) * (m(1, 0) * m(2, 1) - m(1, 1) * m(2, 0));
+  }
+
+  /**
+   * @brief Compute the determinant of an \f$ n \times n \f$ matrix \f$ \mathbf{M} \f$.
+   * 
+   * @details For small matrices (\f$ 1 \times 1 \f$, \f$ 2 \times 2 \f$ or \f$ 3 \times 3 \f$), it directly computes
+   * the determinant using one of the optimized routines nda::linalg::det_1d, nda::linalg::det_2d and 
+   * nda::linalg::det_3d.
+   *
+   * For larger matrices, it calls nda::lapack::getrf and calculates the determinant from its LU decomposition.
+   * 
+   * It throws an exception if the call to nda::lapack::getrf fails.
+   *
+   * @note The matrix \f$ \mathbf{M} \f$ is modified if its number of rows/columns is greater than 3.
+   *
+   * @tparam M nda::Matrix type.
+   * @param m Input/output matrix. On entry, the matrix \f$ \mathbf{M} \f$. On exit, the matrix \f$ \mathbf{M} \f$ or
+   * the LU decomposition of \f$ \mathbf{M} \f$ from nda::lapack::getrf.
+   * @return \f$ \det(\mathbf{M}) \f$.
+   */
+  template <Matrix M>
+    requires(get_algebra<M> == 'M' and nda::mem::have_host_compatible_addr_space<M> and is_blas_lapack_v<get_value_t<M>>)
+  auto det_in_place(M &&m) { // NOLINT (temporary views are allowed here)
+    EXPECTS(is_matrix_square(m));
+
+    // use optimized routines for small matrices, otherwise use LAPACK routine
+    auto const dim = m.shape()[0];
+    if (dim == 1) {
+      return det_1d(m);
+    } else if (dim == 2) {
+      return det_2d(m);
+    } else if (dim == 3) {
+      return det_3d(m);
+    } else if (dim > 3) {
+      // LU factorization with getrf
+      auto ipiv = vector<int, sso<100>>(dim);
+      int info  = nda::lapack::getrf(m, ipiv);
+      if (info != 0) NDA_RUNTIME_ERROR << "Error in nda::linalg::det_in_place: getrf routine failed: info = " << info;
+
+      // calculate the determinant from the LU decomposition
+      auto det    = get_value_t<M>{1};
+      int n_flips = 0;
+      for (int i = 0; i < dim; i++) {
+        det *= m(i, i);
+        // count the number of column interchanges performed by getrf
+        if (ipiv(i) != i + 1) ++n_flips;
+      }
+
+      return ((n_flips % 2 == 1) ? -det : det);
+    } else {
+      // empty matrix
+      return get_value_t<M>{1};
+    }
+  }
+
+  /**
+   * @brief Compute the determinant of an \f$ n \times n \f$ matrix \f$ \mathbf{M} \f$.
+   * 
+   * @details The given matrix/view is not modified. It first makes a copy of the matrix/view and then calls 
+   * nda::linalg::det_in_place.
+   *
+   * @tparam M nda::MemoryMatrix type.
+   * @param m Input matrix. The matrix \f$ \mathbf{M} \f$. 
+   * @return The inverse matrix \f$ \mathbf{M}^{-1} \f$.
+   */
+  template <Matrix M>
+    requires(get_algebra<M> == 'M' and nda::mem::have_host_compatible_addr_space<M> and is_blas_lapack_v<get_value_t<M>>)
+  auto det(M const &m) {
+    EXPECTS(is_matrix_square(m));
     auto m_copy = make_regular(m);
-    return determinant_in_place(m_copy);
+    return det_in_place(m_copy);
   }
 
   namespace clef {
@@ -106,4 +150,4 @@ namespace nda {
 
   /** @} */
 
-} // namespace nda
+} // namespace nda::linalg
