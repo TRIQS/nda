091cc2ef057e8f22888649b7879d20996adeb7b5
Wentzell <Wentzell@users.noreply.github.com>
2025-11-10 19:26:15 +0000

    Deploying to github.io from @ TRIQS/nda@cb518983a22100b86cc83e8b705cd7a28cc4e0f3 🚀

--- docs/unstable/ger_8hpp.js
+++ docs/unstable/ger_8hpp.js
@@ -1,4 +1,5 @@
 var ger_8hpp =
 [
-    [ "nda::blas::ger", "group__linalg__blas.html#gace11f1ec7ce3c54afb76b392d9c98db1", null ]
+    [ "nda::blas::ger", "group__linalg__blas.html#gace11f1ec7ce3c54afb76b392d9c98db1", null ],
+    [ "nda::blas::gerc", "group__linalg__blas.html#ga3e171555b79dd3631823d0fb8ebe7ae8", null ]
 ];
