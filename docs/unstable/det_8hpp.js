efe3d479013404206dd95bd2c4ca7dd8293d99b9
Thoemi09 <thomas.hahn01@gmail.com>
2025-11-19 23:53:44 +0000

    Deploying to github.io from @ TRIQS/nda@685a384bc7c54705996235f6343e94f9803d750b 🚀

--- docs/unstable/det_8hpp.js
+++ docs/unstable/det_8hpp.js
@@ -1,6 +1,7 @@
 var det_8hpp =
 [
-    [ "nda::clef::det", "det_8hpp.html#a905f25b56234f145ca519f5faf51433f", null ],
+    [ "nda::clef::det", "group__clef__expr.html#ga905f25b56234f145ca519f5faf51433f", null ],
+    [ "nda::clef::det", "det_8hpp.html#a3c413c86b4dd95772be5b3d34dcdd960", null ],
     [ "nda::linalg::det", "group__linalg__tools.html#ga3c413c86b4dd95772be5b3d34dcdd960", null ],
     [ "nda::linalg::det_in_place", "group__linalg__tools.html#gaaa18b3b53eaf33a1bffdf759055ec332", null ]
 ];
--- docs/unstable/inv_8hpp.js
+++ docs/unstable/inv_8hpp.js
@@ -1,6 +1,7 @@
 var inv_8hpp =
 [
-    [ "nda::clef::inv", "inv_8hpp.html#a63de7927bdaa8b66b7d68f3e0a42950e", null ],
+    [ "nda::clef::inv", "group__clef__expr.html#ga63de7927bdaa8b66b7d68f3e0a42950e", null ],
+    [ "nda::clef::inv", "inv_8hpp.html#abd0ff5c6c8add0fd6641db2cc5f73a79", null ],
     [ "nda::linalg::inv", "group__linalg__tools.html#gabd0ff5c6c8add0fd6641db2cc5f73a79", null ],
     [ "nda::linalg::inv_in_place", "group__linalg__tools.html#ga5ae8205418692fdc14fc3ee0c5bf322b", null ]
 ];
