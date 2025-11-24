1065880b1053c57d01bf6ad89c4f1d47fa011214
Fikret Ardal <fikret.ardal@tum.de>
2025-11-24 16:55:01 -0500

    add licenses to newly created files

--- c++/nda/simd/simd.hpp
+++ c++/nda/simd/simd.hpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2023--present, The Simons Foundation
+// This file is part of TRIQS/nda and is licensed under the Apache License, Version 2.0.
+// SPDX-License-Identifier: Apache-2.0
+// See LICENSE in the root of this distribution for details.
+
 #pragma once
 
 #include <xsimd/xsimd.hpp>
