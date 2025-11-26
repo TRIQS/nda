7a96f0d8f62f6d13f6ceada0cdbb822f89c98874
Thomas Hahn <thomas.hahn01@gmail.com>
2025-11-26 16:09:19 -0500

    Add eig, eig_in_place, eigvals, eigvals_in_place to nda::linalg

--- c++/nda/linalg.hpp
+++ c++/nda/linalg.hpp
@@ -16,6 +16,7 @@
 #include "./linalg/cross_product.hpp"
 #include "./linalg/det.hpp"
 #include "./linalg/dot.hpp"
+#include "./linalg/eig.hpp"
 #include "./linalg/eigh.hpp"
 #include "./linalg/inv.hpp"
 #include "./linalg/lu.hpp"
