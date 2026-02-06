2f2f45ba76e59eaabfb24f826ecec4c975637a78
Thomas Hahn <thomas.hahn01@gmail.com>
2026-02-06 16:58:44 -0500

    Update svd and svd_in_place in nda::linalg
    - add single precision support
    - use concepts from nda::blas_lapack
    - update docs
    - update tests

--- c++/nda/linalg/svd.hpp
+++ c++/nda/linalg/svd.hpp
@@ -57,32 +57,32 @@ namespace nda::linalg {
    *
    * It first constructs the output vector \f$ \mathbf{s} \f$, which contains the singular values, and the output
    * matrices \f$ \mathbf{U} \f$ and \f$ \mathbf{V}^H \f$. It then calls nda::lapack::gesvd to compute the SVD.
+   * 
+   * The resulting matrices \f$ \mathbf{U} \f$ and \f$ \mathbf{V}^H \f$ have the same memory layout, address space and
+   * value type as \f$ \mathbf{A} \f$. The vector \f$ \mathbf{s} \f$ has the same address space as \f$ \mathbf{A} \f$ 
+   * and its value type is given by `nda::get_fp_t<A>`.
    *
    * An exception is thrown, if the LAPACK call returns a non-zero value.
    *
-   * @note If the input matrix \f$ \mathbf{A} \f$ is in nda::F_layout, the output matrices \f$ \mathbf{U} \f$ and
-   * \f$ \mathbf{V}^H \f$ are also in nda::F_layout. Otherwise, they are in nda::C_layout.
-   *
-   * @tparam A nda::MemoryMatrix type.
+   * @tparam A nda::blas_lapack::BlasArray<2> type.
    * @param a Input/output matrix. On entry, the \f$ m \times n \f$ matrix \f$ \mathbf{A} \f$. On exit, the contents of
    * \f$ \mathbf{A} \f$ are destroyed.
    * @return `std::tuple` containing \f$ \mathbf{U} \f$, \f$ \mathbf{s} \f$ and \f$ \mathbf{V}^H \f$.
    */
-  template <MemoryMatrix A>
-    requires(is_blas_lapack_v<get_value_t<A>>)
+  template <blas_lapack::BlasArray<2> A>
   auto svd_in_place(A &&a) { // NOLINT (temporary views are allowed here)
     using layout_policy       = nda::detail::layout_to_policy<typename std::remove_cvref_t<A>::layout_t>::type;
-    constexpr auto addr_space = nda::mem::get_addr_space<A>;
+    constexpr auto addr_space = mem::get_addr_space<A>;
 
     // vector s and matrices U and V^H
     auto const [m, n] = a.shape();
-    auto s            = vector<double, heap<addr_space>>(std::min(m, n));
+    auto s            = vector<get_fp_t<A>, heap<addr_space>>(std::min(m, n));
     auto U            = matrix<get_value_t<A>, layout_policy, heap<addr_space>>(m, m);
     auto VH           = matrix<get_value_t<A>, layout_policy, heap<addr_space>>(n, n);
 
     // call lapack gesvd
     int info = lapack::gesvd(a, s, U, VH);
-    if (info != 0) NDA_RUNTIME_ERROR << "Error in nda::svd_in_place: gesvd returned a non-zero value: info = " << info;
+    if (info != 0) NDA_RUNTIME_ERROR << "Error in nda::linalg::svd_in_place: gesvd returned a non-zero value: info = " << info;
 
     return std::make_tuple(U, s, VH);
   }
@@ -90,22 +90,12 @@ namespace nda::linalg {
   /**
    * @brief Compute the singular value decomposition (SVD) of a matrix.
    *
-   * @details The function computes the SVD of a given \f$ m \times n \f$ matrix \f$ \mathbf{A} \f$:
-   * \f[
-   *   \mathbf{A} = \mathbf{U} \mathbf{S} \mathbf{V}^H \; ,
-   * \f]
-   * where \f$ \mathbf{U} \f$ is a unitary \f$ m \times m \f$ matrix, \f$ \mathbf{V} \f$ is a unitary \f$ n \times n \f$
-   * matrix and \f$ \mathbf{S} \f$ is an \f$ m \times n \f$ matrix with non-negative real numbers on the diagonal.
-   *
-   * It calls nda::linalg::svd_in_place with a copy of the input matrix \f$ \mathbf{A} \f$.
-   *
-   * @note If the input matrix \f$ \mathbf{A} \f$ is in nda::F_layout, the output matrices \f$ \mathbf{U} \f$ and
-   * \f$ \mathbf{V}^H \f$ are also in nda::F_layout. Otherwise, they are in nda::C_layout.
+   * @details It calls nda::linalg::svd_in_place with a copy of the input matrix \f$ \mathbf{A} \f$.
    * 
-   * @warning This function makes copies of the input arrays/views. When working on the device memory space, this may
+   * @note This function makes copies of the input arrays/views. When working on the device memory space, this may
    * lead to runtime errors if the copying fails.
    *
-   * @tparam A nda::MemoryMatrix type.
+   * @tparam A nda::Matrix type.
    * @param a Input matrix \f$ \mathbf{A} \f$.
    * @return `std::tuple` containing \f$ \mathbf{U} \f$, \f$ \mathbf{s} \f$ and \f$ \mathbf{V}^H \f$.
    */
