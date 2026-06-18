e32cd16c3e4d55fd75bee9ab09ccd6426a67ad86
Wentzell <Wentzell@users.noreply.github.com>
2026-06-18 14:53:32 +0000

    Deploying to github.io from @ TRIQS/nda@2973608753813a00ec412ca6c4d824c7bb92df6e 🚀

--- docs/unstable/gtest__tools_8hpp.js
+++ docs/unstable/gtest__tools_8hpp.js
@@ -2,6 +2,7 @@ var gtest__tools_8hpp =
 [
     [ "EXPECT_ARRAY_EQ", "group__testing.html#gad8ecf010e01059f30570b7797382dc66", null ],
     [ "EXPECT_ARRAY_NEAR", "group__testing.html#ga350f0bafc5dd3ec08c1628a2ac900929", null ],
+    [ "EXPECT_ARRAY_REL_NEAR", "group__testing.html#ga737a9b0efc5dd3c377c6021f02cd8264", null ],
     [ "EXPECT_ARRAY_ZERO", "group__testing.html#gac052fa769b0ec92070b93e813530e70d", null ],
     [ "EXPECT_CLOSE", "group__testing.html#gab283a3d061e6284b03ca50e751ee9d8e", null ],
     [ "EXPECT_COMPLEX_NEAR", "group__testing.html#ga8835b423cd76f0037bee4b70e470ebda", null ],
@@ -9,6 +10,7 @@ var gtest__tools_8hpp =
     [ "array_almost_zero", "group__testing.html#ga7dc5f72bdbee77bbf861d96183d7d615", null ],
     [ "array_are_close", "group__testing.html#gab20d7c4db596019779a8ec209a4db356", null ],
     [ "array_are_equal", "group__testing.html#gaba482f6e79a73794bb4fa4b0bddcfdbd", null ],
+    [ "array_are_rel_close", "group__testing.html#gaa5deb66f4638758dfe5ec04ebd773bcc", null ],
     [ "complex_are_close", "group__testing.html#gaf1255bd37fa39590eb7e02658944f498", null ],
     [ "generic_are_near", "group__testing.html#ga906d8fb962ccc3a6cd46b30231a02e16", null ]
 ];
